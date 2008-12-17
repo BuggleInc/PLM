25b38c3c189e2f5f9706f19fe98b8a1391aba59f
Martin Quinson <martin.quinson@loria.fr>
2008-12-17 10:46:55 +0100

    change exercice title

--- src/lessons/sort/ExInsertionSort.java
+++ src/lessons/sort/ExInsertionSort.java
@@ -8,7 +8,7 @@ public class ExInsertionSort extends SortingExercise {
 
 	public ExInsertionSort(Lesson lesson) {
 		super(lesson);
-		name = "Tri à bulle et variantes";
+		name = "Tris itératifs";
 		SortingWorld[] myWorlds = new SortingWorld[2];
 		myWorlds[0] = new SortingWorld("Functional test",10);
 		myWorlds[1] = new SortingWorld("Performance test",100);
