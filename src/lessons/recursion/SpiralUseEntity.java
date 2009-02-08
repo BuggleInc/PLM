5b6e56bcb6e06b425e7b1d551e526545c780364c
Gerald Oster <oster@loria.fr>
2009-02-08 13:56:24 +0100

    fix a bug in SpiralUseEntity. I did not fix the cause of this bug. Here is the real problem to be fixed : it seems that if you put // in some code of an entity that has to be stripped by the parser (because it belongs to the template and has to be masked to users), then following instructions are also commented out. I guess the problem happens because \n are partially remove by the parser/stripper.

--- src/lessons/recursion/SpiralUseEntity.java
+++ src/lessons/recursion/SpiralUseEntity.java
@@ -5,7 +5,7 @@ public class SpiralUseEntity extends universe.turtles.Turtle {
 
 	public void spiral(int steps, int angle, int length, int increment)	{
 	     if (steps <= 0) {
-	          // do nothing
+	    	 return;
 	     } else {
 	          forward(length);
 	          turnLeft(angle);
@@ -17,10 +17,10 @@ public class SpiralUseEntity extends universe.turtles.Turtle {
 	void doit(int page) {
 		/* BEGIN SOLUTION */
 		switch (page) {
-		case 0:	spiral(100,91,1,2);  break;
-		case 1:	spiral(100,121,1,2); break;
-		case 2: spiral(5,72,100,0);  break;
-		case 3: spiral(5,144,150,0); break;
+		case 0:	spiral(100,90+1,1,2);  break;
+		case 1:	spiral(100,120+1,1,2); break;
+		case 2: spiral(5,360/5,100,0);  break;
+		case 3: spiral(5,2*360/5,150,0); break;
 		case 4: spiral(360,1,1,0);   break;	
 		}
 		/* END SOLUTION */
