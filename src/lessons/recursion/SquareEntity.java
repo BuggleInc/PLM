5f0851b0faf8408336610fae211db474aeedf881
Martin Quinson <martin.quinson@loria.fr>
2009-02-09 14:17:09 +0100

    Met la run() dans le template

--- src/lessons/recursion/SquareEntity.java
+++ src/lessons/recursion/SquareEntity.java
@@ -2,19 +2,21 @@ package lessons.recursion;
 
 public class SquareEntity extends universe.turtles.Turtle {
 
-	/* BEGIN SOLUTION */
+
+	/* BEGIN TEMPLATE */
+	public void run() {
+		/* BEGIN SOLUTION */
+		for (int i = 0; i < 4; i++) {
+			square();
+			turnRight(90);
+		}
+	}
 	public void square() {
 		for (int i = 0; i < 4; i++) {
 			forward(100);
 			turnRight(90);
 		}
-	}
-
-	public void run() {
-		for (int i = 0; i < 4; i++) {
-			square();
-			turnRight(90);
-		}
+    /* END SOLUTION */
 	}
 	/* END TEMPLATE */
 }
