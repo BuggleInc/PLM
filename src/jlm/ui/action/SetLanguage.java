4bd9952bc38ef65fa39a0830ac388b201a5628c3
Martin Quinson <martin.quinson@loria.fr>
2009-03-06 17:40:14 +0100

    forgot to add this file before

---
+++ src/jlm/ui/action/SetLanguage.java
@@ -0,0 +1,27 @@
+package jlm.ui.action;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+
+import javax.swing.ImageIcon;
+
+import jlm.core.Game;
+import jlm.lesson.ExerciseTemplated;
+
+public class SetLanguage extends AbstractGameAction {
+
+	private static final long serialVersionUID = 5778501209753480269L;
+
+	private String lang;
+
+	public SetLanguage(Game game, String text, ImageIcon icon, Component parent, String lang) {
+		super(game, text, icon);
+		this.lang = lang;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		ExerciseTemplated.setLocale(lang);
+	}
+
+}
