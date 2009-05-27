ebe0fa365ede01361955d78c804425abeb6344cb
Martin Quinson <martin.quinson@loria.fr>
2009-05-27 14:41:58 +0200

    Autoscale the hanoi view

--- src/universe/hanoi/HanoiWorldView.java
+++ src/universe/hanoi/HanoiWorldView.java
@@ -26,15 +26,20 @@ public class HanoiWorldView extends WorldView {
 	public void paintComponent(Graphics g) {
 		HanoiWorld hw = (HanoiWorld)world;
 		super.paintComponent(g);
-//		System.err.println("display "+hw);
-
+		
 		Graphics2D g2 = (Graphics2D) g;
 
 		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		
+		double renderedX = 300.;
+		double renderedY = 250.;		
+		double ratio = Math.min(((double) getWidth()) / renderedX, ((double) getHeight()) / renderedY);
+		g2.translate(Math.abs(getWidth() - ratio * renderedX)/2., Math.abs(getHeight() - ratio * renderedY)/2.);
+		g2.scale(ratio, ratio);
+		
 		/* clear board */
 		g2.setColor(Color.white);
-		g2.fill(new Rectangle2D.Double(0., 0., 300., 250.));
+		g2.fill(new Rectangle2D.Double(0., 0., renderedX, renderedY));
 		
 		
 		drawSlot(g2,hw.values(0), 50.);
@@ -45,7 +50,7 @@ public class HanoiWorldView extends WorldView {
 	private void drawSlot(Graphics2D g2, Integer[] values, double xoffset) {
 		/* draw bar */
 		g2.setColor(Color.black);
-		g2.fill(new Rectangle2D.Double(  xoffset-2, 55.,  2., 125.));
+		g2.fill(new Rectangle2D.Double(xoffset-2, 55.,  2., 125.));
 		
 		/* draw discs */
 		int height = 1;
