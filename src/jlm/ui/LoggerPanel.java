c1d85f68c2e4c1a9e7463495c9f1a4cbc1317ab2
Martin Quinson <martin.quinson@loria.fr>
2009-09-16 16:30:22 +0200

    Re-add the file name and line number to the error messages comming from the compiler

--- src/jlm/ui/LoggerPanel.java
+++ src/jlm/ui/LoggerPanel.java
@@ -31,7 +31,7 @@ public class LoggerPanel extends JTextArea implements LogWriter {
 	@Override
 	public void log(DiagnosticCollector<JavaFileObject> diagnostics) {
 		for (Diagnostic<? extends JavaFileObject> diagnostic : diagnostics.getDiagnostics()) {			
-			append(diagnostic.getMessage(null));
+			append(diagnostic.getSource().getName()+":"+diagnostic.getLineNumber()+":"+ diagnostic.getMessage(null));
 			append("\n");
 		}
 	}
