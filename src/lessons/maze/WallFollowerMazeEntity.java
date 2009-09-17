f7bbbcff115c3b1415ae44f7377c3ebfbb344d13
Martin Quinson <martin.quinson@loria.fr>
2009-09-17 18:28:37 +0200

    Add my solution in comments (for now)

--- src/lessons/maze/WallFollowerMazeEntity.java
+++ src/lessons/maze/WallFollowerMazeEntity.java
@@ -16,7 +16,37 @@ public class WallFollowerMazeEntity extends universe.bugglequest.SimpleBuggle {
 		if (isInited())
 			throw new RuntimeException("setPos(int,int) forbidden in this exercise");
 	}
+/* Solution by Martin
+	// Stop moving if stepping over a baggle 
+	public void forward() {
+	    if (!isOverBaggle()) 
+	      super.forward();
+	}
 
+	public void stepHandOnWall() {
+	    // PRE: we have a wall on the left
+	    // POST: we still have the same wall on the left, are one step ahead
+
+	   while (!isFacingWall()) {
+	       forward();
+	       turnLeft(); // change to turnRight to get a right follower
+	   }
+	   turnRight(); // change to turnLeft to get a right follower
+	}
+
+	public void run() {
+	    // Make sure we have a wall to the left
+	    turnLeft();
+	    while (!isFacingWall())
+	      forward();
+	    turnRight();
+	   
+	    while (!isOverBaggle())
+	      stepHandOnWall();
+	      
+	    pickUpBaggle();
+	}
+*/
 	/* BEGIN SOLUTION */
 	public void run() {
 		putHandOnSideWall();
