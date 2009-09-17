363871b8d0ab2202af578b1fff0837aa222eccb0
Gerald Oster <oster@loria.fr>
2009-09-17 18:29:35 +0200

    protecting traversal entities against forward, backward, isFacingWall, ... method calls

--- src/lessons/welcome/TraversalByLineEntity.java
+++ src/lessons/welcome/TraversalByLineEntity.java
@@ -39,4 +39,52 @@ public class TraversalByLineEntity extends SimpleBuggle {
 	}
 
 	/* END TEMPLATE */	
+	
+    @Override
+	public void forward()  {
+        if (isInited())
+                throw new RuntimeException("forward() forbidden in this exercise");
+    }
+    @Override
+	public void forward(int step)  {
+        if (isInited())
+                throw new RuntimeException("forward(int step) forbidden in this exercise");
+    }
+    @Override
+    public void backward()  {
+        if (isInited())
+                throw new RuntimeException("backward() forbidden in this exercise");
+    }
+    @Override
+    public void backward(int step)  {
+        if (isInited())
+                throw new RuntimeException("backward(int step) forbidden in this exercise");
+    }
+    @Override
+    public void turnLeft()  {
+        if (isInited())
+                throw new RuntimeException("turnLeft() forbidden in this exercise");
+    }
+    @Override
+    public void turnRight()  {
+        if (isInited())
+                throw new RuntimeException("turnRight() forbidden in this exercise");
+    }
+    @Override
+    public void turnBack()  {
+        if (isInited())
+                throw new RuntimeException("turnBack() forbidden in this exercise");
+    }
+    @Override
+       public boolean isFacingWall()  {
+        if (isInited())
+                throw new RuntimeException("isFacingWall() forbidden in this exercise");
+        return false;
+    }
+    @Override
+    public boolean isBackingWall()  {
+        if (isInited())
+                throw new RuntimeException("isBackingWall() forbidden in this exercise");
+        return false;
+    }
 }
