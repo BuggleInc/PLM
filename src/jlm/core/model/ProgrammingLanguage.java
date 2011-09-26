497b2bd8ee8fb9d3203bc30a469879d046585931
Martin Quinson <martin.quinson@loria.fr>
2011-09-26 22:11:46 +0200

    Make ProgrammingLanguage comparable so that we can build a map of it -- I should test my code before commiting, sorry

--- src/jlm/core/model/ProgrammingLanguage.java
+++ src/jlm/core/model/ProgrammingLanguage.java
@@ -1,6 +1,6 @@
 package jlm.core.model;
 
-public class ProgrammingLanguage {
+public class ProgrammingLanguage implements Comparable<ProgrammingLanguage> {
 	String lang;
 	String ext;
 	public ProgrammingLanguage(String l, String ext) {
@@ -27,5 +27,14 @@ public class ProgrammingLanguage {
 	@Override
 	public int hashCode() {
 		return lang.hashCode();
+	}
+	@Override
+	public int compareTo(ProgrammingLanguage o) {
+		if (o == null)
+			return 1;
+		int res = lang.compareTo(o.lang);
+		if (res != 0)
+			return res;
+		return ext.compareTo(o.ext);
 	}
 }
