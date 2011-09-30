fb272473d5756e7ba01cd9a3c754cb5562f2a432
Martin Quinson <martin.quinson@loria.fr>
2011-09-30 11:09:19 +0200

    add a python entity for this exercise

---
+++ src/lessons/welcome/BasicsDrawGEntity.py
@@ -0,0 +1,22 @@
+# BEGIN TEMPLATE 
+		brushDown()
+		turnLeft()
+		forward(4)
+		turnLeft()
+		forward(4)
+		turnLeft()
+		forward(4)
+		turnLeft()
+		forward()
+		forward()
+		turnLeft()
+		forward()
+#		 back home 
+		brushUp()
+		turnRight()
+		forward(2)
+		turnRight()
+		forward()
+		turnLeft()
+# END TEMPLATE
+
