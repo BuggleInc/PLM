e4f44176ae70cecfbfff46daeae77b5b59c664e7
Julien Guépin <chmod0@chmod0-desktop.(none)>
2012-02-14 23:05:39 +0100

    Send username to alert servlet

--- src/jlm/core/ui/action/Alert.java
+++ src/jlm/core/ui/action/Alert.java
@@ -9,13 +9,21 @@ import java.io.OutputStreamWriter;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.URLEncoder;
 
 public class Alert extends AbstractGameAction {
 
     private URL server;
+    private String username;
 
     public Alert(Game game, String text, ImageIcon icon) {
         super(game, text, icon);
+
+        username = System.getenv("USER");
+        if (username == null)
+            username = System.getenv("USERNAME");
+        if (username == null)
+            username = "John Doe";
 
         try {
             server = new URL(Game.getProperty("jlm.appengine.url") + "/alert");
@@ -27,10 +35,11 @@ public class Alert extends AbstractGameAction {
     @Override
     public void actionPerformed(ActionEvent e) {
         try {
+            String data = URLEncoder.encode("username", "UTF-8") + "=" + URLEncoder.encode(username, "UTF-8");
             URLConnection conn = server.openConnection();
             conn.setDoOutput(true);
             OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
-            wr.write("");
+            wr.write(data);
             wr.flush();
         } catch (IOException e1) {
             e1.printStackTrace();
