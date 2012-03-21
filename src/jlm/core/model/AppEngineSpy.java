3111c50d2b688aaa1f73cd5005514f4faf3aa7e9
Julien Guepin <julien@guepin.fr>
2012-03-21 16:12:24 +0100

    * added json-java library * rewrote the serverspy abstraction class (override sendRequest method to send the request to another server)

--- src/jlm/core/model/AppEngineSpy.java
+++ src/jlm/core/model/AppEngineSpy.java
@@ -1,27 +1,19 @@
 package jlm.core.model;
 
-import jlm.core.model.lesson.ExecutionProgress;
-import jlm.core.model.lesson.Exercise;
-
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.net.URLEncoder;
-import java.util.HashMap;
-import java.util.Set;
 
-public class AppEngineSpy implements ServerSpy {
+public class AppEngineSpy extends ServerSpy {
     
-    private String username;
     private URL server;
 
     public AppEngineSpy() {
-        username = System.getenv("USER");
-        if (username == null)
-            username = System.getenv("USERNAME");
-        if (username == null)
-            username = "John Doe";
+        super();
 
         try {
             server = new URL(Game.getProperty("jlm.appengine.url") + "/student");
@@ -30,58 +22,14 @@ public class AppEngineSpy implements ServerSpy {
         }
     }
     
-    
-    @Override
-    public void executed(Exercise exo) {
-    	
-        HashMap<String, String> parameters = new HashMap<String, String>();
-        Game game = Game.getInstance();
-    	
-        ExecutionProgress lastResult = exo.lastResult;
-        
-        // Retrieve appropriate parameters regarding the current exercise
-        parameters.put("username", 		username );
-        parameters.put("course", 	game.getCourseID() );
-        
-        parameters.put("exoname", 		exo.getName() );
-        parameters.put("exolang", 		lastResult.language.toString() );
-        parameters.put("passedtests", 	lastResult.passedTests +"" );
-        parameters.put("totaltests", 	lastResult.totalTests +"" );
-        
-        
-        try {
-        	// Construct request query
-        	String query = "";
-        	
-        	Set<String> paramKeys = parameters.keySet();
-        	boolean putDelim = false;
-        	for (String key : paramKeys) 
-        	{      		
-        		if (putDelim) query += "&";
-        		else putDelim = true;
-        		
-        		query += key + "=" + URLEncoder.encode(parameters.get(key), "UTF-8");
-        	}
-        	
-        	this.sendQuery(query);
-        	System.out.println(query);
-        	
-        } catch (UnsupportedEncodingException e) {
-	        e.printStackTrace();
-	    }
-        
-    }
-    
-    
-    
-    public void sendQuery(String query) {
+    public void sendRequest(String request) {
     	try {
     		
 	    	// Send data
 	        URLConnection conn = server.openConnection();
 	        conn.setDoOutput(true);
 	        OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
-	        wr.write(query);
+	        wr.write(request);
 	        wr.flush();
 	
 	        // Get response data and print it
--- src/jlm/core/model/ServerSpy.java
+++ src/jlm/core/model/ServerSpy.java
@@ -1,10 +1,43 @@
 package jlm.core.model;
 
+import jlm.core.model.json.JSONException;
+import jlm.core.model.json.JSONObject;
+import jlm.core.model.lesson.ExecutionProgress;
 import jlm.core.model.lesson.Exercise;
 
-public interface ServerSpy extends ProgressSpyListener {
-    
-    public void executed(Exercise exo);
-    
-    public void sendQuery(String query);
+public abstract class ServerSpy implements ProgressSpyListener {
+
+    protected String username;
+
+    public ServerSpy() {
+        username = System.getenv("USER");
+        if (username == null)
+            username = System.getenv("USERNAME");
+        if (username == null)
+            username = "John Doe";
+    }
+
+    public void executed(Exercise exo) {
+        JSONObject jsonObject = new JSONObject();
+
+        Game game = Game.getInstance();
+        ExecutionProgress lastResult = exo.lastResult;
+
+        try {
+            // Retrieve appropriate parameters regarding the current exercise
+            jsonObject.put("username", username);
+            jsonObject.put("course", game.getCourseID());
+            jsonObject.put("exoname", exo.getName());
+            jsonObject.put("exolang", lastResult.language.toString());
+            jsonObject.put("passedtests", lastResult.passedTests + "");
+            jsonObject.put("totaltests", lastResult.totalTests + "");
+
+            sendRequest(jsonObject.toString());
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public abstract void sendRequest(String request);
 }
