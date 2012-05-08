105dec6e9f74a46bd1c4fa432eec058893d984da
Guillaume <guillaume.gerard@esial.net>
2012-05-09 00:14:35 +0200

    Change org.json Library (Don't Be Evil Licence) by JSon Smart (Apache License 2.0)

--- src/jlm/core/model/ServerSpy.java
+++ src/jlm/core/model/ServerSpy.java
@@ -1,128 +1,106 @@
 package jlm.core.model;
 
-import org.json.me.JSONException;
-import org.json.me.JSONObject;
-
+import net.minidev.json.JSONObject;
 import jlm.core.model.lesson.ExecutionProgress;
 import jlm.core.model.lesson.Exercise;
 
 /**
- * Abstract class to send users results to a server
- * It constructs the request to send, you have to extend this class to indicate how/where to send the request
+ * Abstract class to send users results to a server It constructs the request to
+ * send, you have to extend this class to indicate how/where to send the request
  */
 public abstract class ServerSpy implements ProgressSpyListener {
 
-    protected String username;
+	protected String username;
 
-    public ServerSpy() {
-        username = System.getenv("USER");
-        if (username == null)
-            username = System.getenv("USERNAME");
-        if (username == null)
-            username = "John Doe";
-    }
+	public ServerSpy() {
+		username = System.getenv("USER");
+		if (username == null)
+			username = System.getenv("USERNAME");
+		if (username == null)
+			username = "John Doe";
+	}
 
-    /**
-     * Receive an exercise progress from progressSpyListener, and construct the request to send to
-     * a server in json
-     * @param exo progress data
-     */
-    @Override
-    public void executed(Exercise exo) {
-        JSONObject jsonObject = new JSONObject();
+	/**
+	 * Receive an exercise progress from progressSpyListener, and construct the
+	 * request to send to a server in json
+	 * 
+	 * @param exo
+	 *            progress data
+	 */
+	@Override
+	public void executed(Exercise exo) {
+		JSONObject jsonObject = new JSONObject();
 
-        Game game = Game.getInstance();
-        ExecutionProgress lastResult = exo.lastResult;
-        String exoCode = exo.getPublicSourceFile(lastResult.language, 0).getBody();
+		Game game = Game.getInstance();
+		ExecutionProgress lastResult = exo.lastResult;
+		String exoCode = exo.getPublicSourceFile(lastResult.language, 0)
+				.getBody();
 
-        try {
-            // Retrieve appropriate parameters regarding the current exercise
-            jsonObject.put("username", username);
-            jsonObject.put("course", game.getCourseID());
-            jsonObject.put("exoname", exo.getName());
-            jsonObject.put("exolang", lastResult.language.toString());
-            jsonObject.put("passedtests", lastResult.passedTests + "");
-            jsonObject.put("totaltests", lastResult.totalTests + "");
-            jsonObject.put("source", exoCode);
-            jsonObject.put("action", "executed");
+		// Retrieve appropriate parameters regarding the current exercise
+		jsonObject.put("username", username);
+		jsonObject.put("course", game.getCourseID());
+		jsonObject.put("exoname", exo.getName());
+		jsonObject.put("exolang", lastResult.language.toString());
+		jsonObject.put("passedtests", lastResult.passedTests + "");
+		jsonObject.put("totaltests", lastResult.totalTests + "");
+		jsonObject.put("source", exoCode);
+		jsonObject.put("action", "executed");
 
-            sendRequest(jsonObject.toString());
+		sendRequest(jsonObject.toString());
 
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-    }
+	}
 
-    @Override
-    public void switched(Exercise exo) {
-        JSONObject jsonObject = new JSONObject();
+	@Override
+	public void switched(Exercise exo) {
+		JSONObject jsonObject = new JSONObject();
 
-        Game game = Game.getInstance();
-        ExecutionProgress lastResult = exo.lastResult;
+		Game game = Game.getInstance();
+		ExecutionProgress lastResult = exo.lastResult;
+		// Retrieve appropriate parameters regarding the new exercise
+		jsonObject.put("username", username);
+		jsonObject.put("course", game.getCourseID());
+		jsonObject.put("exoname", exo.getName());
+		jsonObject.put("exolang", lastResult.language.toString());
+		jsonObject.put("action", "switched");
 
-        try {
-            // Retrieve appropriate parameters regarding the new exercise
-            jsonObject.put("username", username);
-            jsonObject.put("course", game.getCourseID());
-            jsonObject.put("exoname", exo.getName());
-            jsonObject.put("exolang", lastResult.language.toString());
-            jsonObject.put("action", "switched");
+		sendRequest(jsonObject.toString());
+	}
 
-            sendRequest(jsonObject.toString());
+	/**
+	 * Send a presence report to the server
+	 */
+	@Override
+	public void heartbeat() {
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("username", username);
+		jsonObject.put("action", "heartbeat");
 
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-    }
+		sendRequest(jsonObject.toString());
+	}
 
-    /**
-     * Send a presence report to the server
-     */
-    @Override
-    public void heartbeat(){
-        JSONObject jsonObject = new JSONObject();
+	@Override
+	public void join() {
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("username", username);
+		jsonObject.put("action", "join");
 
-        try{
-            jsonObject.put("username", username);
-            jsonObject.put("action", "heartbeat");
+		sendRequest(jsonObject.toString());
+	}
 
-            sendRequest(jsonObject.toString());
-        } catch(JSONException e){
-            e.printStackTrace();
-        }
-    }
+	@Override
+	public void leave() {
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("username", username);
+		jsonObject.put("action", "leave");
 
-    @Override
-    public void join(){
-        JSONObject jsonObject = new JSONObject();
+		sendRequest(jsonObject.toString());
+	}
 
-        try{
-            jsonObject.put("username", username);
-            jsonObject.put("action", "join");
-
-            sendRequest(jsonObject.toString());
-        } catch(JSONException e){
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public void leave(){
-        JSONObject jsonObject = new JSONObject();
-
-        try{
-            jsonObject.put("username", username);
-            jsonObject.put("action", "leave");
-
-            sendRequest(jsonObject.toString());
-        } catch(JSONException e){
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * Method to implement to indicate how/where to send data
-     * @param request request in json to send to the server
-     */
-    public abstract void sendRequest(String request);
+	/**
+	 * Method to implement to indicate how/where to send data
+	 * 
+	 * @param request
+	 *            request in json to send to the server
+	 */
+	public abstract void sendRequest(String request);
 }
