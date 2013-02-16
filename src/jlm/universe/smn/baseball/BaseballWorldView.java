c17426cc231007eafe41796b5ff0dbd889501634
Julien <julienbastian@yahoo.fr>
2013-02-16 19:50:21 +0100

    Improve the arrow representing a movement during a baseball game

--- src/jlm/universe/smn/baseball/BaseballWorldView.java
+++ src/jlm/universe/smn/baseball/BaseballWorldView.java
@@ -34,43 +34,56 @@ public class BaseballWorldView extends WorldView
 	}
 	
 	/**
-	 * Draw the component of the world
-	 * @param g The Graphics2D context to draw on
-	 */
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		
-		Graphics2D g2 = (Graphics2D) g;
+	   * Draws an arrow on the given Graphics2D context
+	   * ( adapted from http://www.bytemycode.com/snippets/snippet/82/ )
+	   * @param g The Graphics2D context to draw on
+	   * @param xTail The x location of the "tail" of the arrow
+	   * @param yTail The y location of the "tail" of the arrow
+	   * @param xHead The x location of the "head" of the arrow
+	   * @param yHead The y location of the "head" of the arrow
+	   */
+	  private void drawArrow( Graphics2D g, int xTail, int yTail, int xHead, int yHead)
+	  {
+	    float arrowWidth = 12.0f ;
+	    float theta = 0.6f ;
+	    int[] xPoints = new int[ 3 ] ;
+	    int[] yPoints = new int[ 3 ] ;
+	    float[] vecLine = new float[ 2 ] ;
+	    float[] vecLeft = new float[ 2 ] ;
+	    float fLength;
+	    float th;
+	    float ta;
+	    float baseX, baseY ;
 
-		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		
-		double renderedX = 300.;
-		double renderedY = 300.;		
-		double ratio = Math.min(((double) getWidth()) / renderedX, ((double) getHeight()) / renderedY);
-		g2.translate(Math.abs(getWidth() - ratio * renderedX)/2., Math.abs(getHeight() - ratio * renderedY)/2.);
-		g2.scale(ratio, ratio);
-		
-		/* drawn the field */
-		g2.setColor(new Color(58,157,35)); // gazon
-		g2.fill(new Rectangle2D.Double(0., 0., renderedX, renderedY));
-		
-		int radius = 120 ;
-		int[] fieldCenter = {(int) renderedX/2, (int) renderedY/2};
-		drawDisk(g2, fieldCenter , radius+30, new Color(174,74,52));
-		
-		BaseballWorld myWorld = (BaseballWorld) this.world ;
-		int amountOfBases = myWorld.getAmountOfBases();
-		double theta = 2*Math.PI / amountOfBases;
-		int L = Math.max(3*(20-amountOfBases),10); // adapting the size of the base to the total amount of bases
-		radius+=amountOfBases-5; // adapting the position of the base to the total amount of bases
-		
-		for ( int i=0 ; i < myWorld.getAmountOfBases();i++)
-		{
-			drawBase(g2, L, radius, theta*i, (int) renderedX/2, (int) renderedY/2 , myWorld.field.getBase(i),amountOfBases);
-		}
-		
-		drawLastMove(g2, myWorld.getLastMove(), radius-L/2-3, theta, renderedX/2, renderedY/2, L);
-	}
+	    xPoints[ 0 ] = xHead ;
+	    yPoints[ 0 ] = yHead ;
+
+	    // build the line vector
+	    vecLine[ 0 ] = (float)xPoints[ 0 ] - xTail ;
+	    vecLine[ 1 ] = (float)yPoints[ 0 ] - yTail ;
+
+	    // build the arrow base vector - normal to the line
+	    vecLeft[ 0 ] = -vecLine[ 1 ] ;
+	    vecLeft[ 1 ] = vecLine[ 0 ] ;
+
+	    // setup length parameters
+	    fLength = (float)Math.sqrt( vecLine[0] * vecLine[0] + vecLine[1] * vecLine[1] ) ;
+	    th = arrowWidth / ( 2.0f * fLength ) ;
+	    ta = arrowWidth / ( 2.0f * ( (float)Math.tan( theta ) / 2.0f ) * fLength ) ;
+
+	    // find the base of the arrow
+	    baseX = ( (float)xPoints[ 0 ] - ta * vecLine[0]);
+	    baseY = ( (float)yPoints[ 0 ] - ta * vecLine[1]);
+
+	    // build the points on the sides of the arrow
+	    xPoints[ 1 ] = (int)( baseX + th * vecLeft[0] );
+	    yPoints[ 1 ] = (int)( baseY + th * vecLeft[1] );
+	    xPoints[ 2 ] = (int)( baseX - th * vecLeft[0] );
+	    yPoints[ 2 ] = (int)( baseY - th * vecLeft[1] );
+
+	    g.drawLine( xTail, yTail, (int)baseX, (int)baseY ) ;
+	    g.fillPolygon( xPoints, yPoints, 3 ) ;
+	  }
 	
 	/**
 	 * Draw the base and the players
@@ -96,7 +109,7 @@ public class BaseballWorldView extends WorldView
 		centerPlayerOne[1] = (int) (y-((L/2)*Math.cos(theta)+r*Math.sin(theta))); // y coordinate of the center of the disk representing player one
 		  
 		Color colorPlayerOne = obtainColor(base.getPlayer(0).getColor());
-		drawDisk(g, centerPlayerOne, radius, colorPlayerOne);
+		drawDisk(g, centerPlayerOne, radius, colorPlayerOne,"1");
 		  
 		// draw Player Two
 		int centerPlayerTwo[] = new int[2];
@@ -105,7 +118,87 @@ public class BaseballWorldView extends WorldView
 		centerPlayerTwo[1] = (int) (y-(-(L/2)*Math.cos(theta)+r*Math.sin(theta))); // y coordinate of the center of the disk representing player two
 		
 		Color colorPlayerTwo = obtainColor(base.getPlayer(1).getColor());
-		drawDisk(g, centerPlayerTwo, radius, colorPlayerTwo);
+		drawDisk(g, centerPlayerTwo, radius, colorPlayerTwo,"2");
+	}
+
+	/**
+	 * Draw a disk which represent the player
+	 * @param g The Graphics2D context to draw on
+	 * @param centerPlayerOne the center of the disk which will represent the player
+	 * @param color the color with which the disk shall be filled
+	 */
+	private void drawDisk(Graphics2D g, int[] center, int radius,Color color, String s) {
+		g.setColor(Color.BLACK);
+		g.drawOval(center[0]-radius, center[1]-radius, radius*2, radius*2);
+		g.setColor(color);
+		g.fillOval(center[0]-radius, center[1]-radius, radius*2, radius*2);
+	}
+	
+	/**
+	 * Draw an arrow which represent the last movement
+	 * @param g The Graphics2D context to draw on
+	 * @param move The baseball move that we want to draw
+	 * @param radius The distance between the center of the screen and end of the line
+	 * @param theta The default angle between two bases
+	 * @param xControl The center of the screen in x-axis
+	 * @param yControl The center of the screen in y-axis
+	 * @param L A coefficient which adapt the length of the arrow to the total amount of bases
+	 */
+	private void drawLastMove(Graphics2D g, BaseballMove move, int radius, double theta, double xControl, double yControl , int L){
+		if ( move != null)
+		{
+			Stroke s = g.getStroke();
+			double xBegin = 0 ;
+			double yBegin = 0 ;
+			double xEnd = 0 ;
+			double yEnd = 0 ;
+			int yHead=0;
+			int xHead=0;
+			
+			double thetaSrc = move.getBaseSrc() * theta ;
+			double thetaDst = move.getBaseDst() * theta ;
+			int radiusEnd = radius-15;
+			
+			g.setColor(obtainColor(move.getPlayerColor()));
+			g.setStroke(new BasicStroke(
+					3.0f,						// Width
+					BasicStroke.CAP_ROUND,		// End cap
+                    BasicStroke.JOIN_BEVEL,		// Join style
+                    10.0f,						// Miter limit
+                    new float[] {5.0f,5.0f},	// Dash pattern
+                    0.0f						// Dash phase
+                    ));
+			if ( move.getPlayerSrc() == 0)
+			{
+				xBegin = xControl+radius*Math.cos(thetaSrc)-(L/2)*Math.sin(thetaSrc);
+				yBegin = yControl-((L/2)*Math.cos(thetaSrc)+radius*Math.sin(thetaSrc))  ;
+			}
+			else
+			{
+				xBegin= xControl+radius*Math.cos(thetaSrc)+(L/2)*Math.sin(thetaSrc);
+				yBegin= yControl-(-(L/2)*Math.cos(thetaSrc)+radius*Math.sin(thetaSrc)) ;
+			}
+			if ( move.getPlayerDst() == 0)
+			{
+				xEnd =  xControl+radiusEnd*Math.cos(thetaDst)-(L/2)*Math.sin(thetaDst);
+				yEnd =  yControl-((L/2)*Math.cos(thetaDst)+radiusEnd*Math.sin(thetaDst));
+				
+				xHead =  (int) (xControl+radius*Math.cos(thetaDst)-(L/2)*Math.sin(thetaDst));
+				yHead =  (int) (yControl-((L/2)*Math.cos(thetaDst)+radius*Math.sin(thetaDst)));
+			}
+			else
+			{
+				xEnd= xControl+radiusEnd*Math.cos(thetaDst)+(L/2)*Math.sin(thetaDst);
+				yEnd =yControl-(-(L/2)*Math.cos(thetaDst)+radiusEnd*Math.sin(thetaDst)) ;
+				
+				xHead= (int) (xControl+radius*Math.cos(thetaDst)+(L/2)*Math.sin(thetaDst));
+				yHead =(int) (yControl-(-(L/2)*Math.cos(thetaDst)+radius*Math.sin(thetaDst))) ;
+			}
+			g.draw(new QuadCurve2D.Double(xBegin, yBegin, xControl, yControl, xEnd, yEnd));
+			g.setStroke(s);
+
+			drawArrow(g,(int) xEnd, (int) yEnd, xHead, yHead);
+		}
 	}
 
 	/**
@@ -139,78 +232,14 @@ public class BaseballWorldView extends WorldView
 		g.setColor(Color.BLACK);
 		g.drawPolygon(xPoints, yPoints, amountOfPoints);
 	}
-
-	/**
-	 * Draw a disk which represent the player
-	 * @param g The Graphics2D context to draw on
-	 * @param centerPlayerOne the center of the disk which will represent the player
-	 * @param color the color with which the disk shall be filled
-	 */
-	private void drawDisk(Graphics2D g, int[] center, int radius,Color color) {
-		g.setColor(Color.BLACK);
-		g.drawOval(center[0]-radius, center[1]-radius, radius*2, radius*2);
-		g.setColor(color);
-		g.fillOval(center[0]-radius, center[1]-radius, radius*2, radius*2);
-	}
 	
 	/**
-	 * Draw an arrow which represent the last movement
-	 * @param g The Graphics2D context to draw on
-	 * @param move The baseball move that we want to draw
-	 * @param radius The distance between the center of the screen and end of the line
-	 * @param theta The default angle between two bases
-	 * @param xControl The center of the screen in x-axis
-	 * @param yControl The center of the screen in y-axis
-	 * @param L A coefficient which adapt the length of the arrow to the total amount of bases
+	 * Return the icon of the world
+	 * @return the icon for the exercise selection
 	 */
-	private void drawLastMove(Graphics2D g, BaseballMove move, int radius, double theta, double xControl, double yControl , int L){
-		if ( move != null)
-		{
-			Stroke s = g.getStroke();
-			double xBegin = 0 ;
-			double yBegin = 0 ;
-			double xEnd = 0 ;
-			double yEnd = 0 ;
-			double thetaBegin = move.getBaseSrc() * theta ;
-			double thetaEnd = move.getBaseDst() * theta ;
-			
-			g.setColor(obtainColor(move.getPlayerColor()));
-			g.setStroke(new BasicStroke(
-					3.0f,						// Width
-					BasicStroke.CAP_ROUND,		// End cap
-                    BasicStroke.JOIN_BEVEL,		// Join style
-                    10.0f,						// Miter limit
-                    new float[] {5.0f,5.0f},	// Dash pattern
-                    0.0f						// Dash phase
-                    ));
-			if ( move.getPlayerSrc() == 0)
-			{
-				xBegin = xControl+radius*Math.cos(thetaBegin)-(L/2)*Math.sin(thetaBegin);
-				yBegin = yControl-((L/2)*Math.cos(thetaBegin)+radius*Math.sin(thetaBegin));
-				thetaBegin -= theta/4;
-			}
-			else
-			{
-				xBegin = xControl+radius*Math.cos(thetaBegin)+(L/2)*Math.sin(thetaBegin);
-				yBegin = yControl-(-(L/2)*Math.cos(thetaBegin)+radius*Math.sin(thetaBegin)) ;
-				thetaBegin += theta/4;
-			}
-			if ( move.getPlayerDst() == 0)
-			{
-				xEnd = xControl+radius*Math.cos(thetaEnd)-(L/2)*Math.sin(thetaEnd);;
-				yEnd = yControl-((L/2)*Math.cos(thetaEnd)+radius*Math.sin(thetaEnd));
-				thetaEnd -= theta/4;
-			}
-			else
-			{
-				xEnd = xControl+radius*Math.cos(thetaEnd)+(L/2)*Math.sin(thetaEnd);
-				yEnd = yControl-(-(L/2)*Math.cos(thetaEnd)+radius*Math.sin(thetaEnd)) ;
-				thetaEnd += theta/4;
-			}
-			
-			g.draw(new QuadCurve2D.Double(xBegin, yBegin, xControl, yControl, xEnd, yEnd));
-			g.setStroke(s);
-		}
+	public ImageIcon getIcon()
+	{
+		return ResourcesCache.getIcon("resources/IconWorld/baseball.png");
 	}
 	
 	/**
@@ -280,12 +309,42 @@ public class BaseballWorldView extends WorldView
 	}
 	
 	/**
-	 * Return the icon of the world
-	 * @return the icon for the exercise selection
+	 * Draw the component of the world
+	 * @param g The Graphics2D context to draw on
 	 */
-	public ImageIcon getIcon()
-	{
-		return ResourcesCache.getIcon("resources/IconWorld/baseball.png");
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		Graphics2D g2 = (Graphics2D) g;
+
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		
+		double renderedX = 300.;
+		double renderedY = 300.;		
+		double ratio = Math.min(((double) getWidth()) / renderedX, ((double) getHeight()) / renderedY);
+		g2.translate(Math.abs(getWidth() - ratio * renderedX)/2., Math.abs(getHeight() - ratio * renderedY)/2.);
+		g2.scale(ratio, ratio);
+		
+		/* drawn the field */
+		g2.setColor(new Color(58,157,35)); // gazon
+		g2.fill(new Rectangle2D.Double(0., 0., renderedX, renderedY));
+		
+		int radius = 120 ;
+		int[] fieldCenter = {(int) renderedX/2, (int) renderedY/2};
+		drawDisk(g2, fieldCenter , radius+30, new Color(174,74,52),"");
+		
+		BaseballWorld myWorld = (BaseballWorld) this.world ;
+		int amountOfBases = myWorld.getAmountOfBases();
+		double theta = 2*Math.PI / amountOfBases;
+		int L = Math.max(3*(20-amountOfBases),10); // adapting the size of the base to the total amount of bases
+		radius+=amountOfBases-5; // adapting the position of the base to the total amount of bases
+		
+		for ( int i=0 ; i < myWorld.getAmountOfBases();i++)
+		{
+			drawBase(g2, L, radius, theta*i, (int) renderedX/2, (int) renderedY/2 , myWorld.field.getBase(i),amountOfBases);
+		}
+		
+		drawLastMove(g2, myWorld.getLastMove(), radius-L/2-3, theta, renderedX/2, renderedY/2, L);
 	}
 
 }
