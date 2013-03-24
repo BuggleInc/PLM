d616d22ccd0e3151715cec568c55815444c8a9e3
g-humbert <geoffrey.humbert@esial.net>
2013-03-24 10:34:07 +0000

    Add .py files

---
+++ src/lessons/maze/IslandMazeEntity.py
@@ -0,0 +1,63 @@
+# BEGIN SOLUTION 
+  public void run() {
+		chosenD = Direction.NORTH;
+		setDirection(chosenD);
+        
+		while (!isOverBaggle()) {
+			while (!isFacingWall()) {
+				forward();
+			}
+			turnLeft();
+            
+			do {
+				keepHandOnSideWall();
+			} while (!(angleSum == 0 && isChosenDirectionFree()) && !isOverBaggle());
+		}
+		
+		pickUpBaggle();
+	}
+    
+	int angleSum = 0;
+	Direction chosenD;
+	Direction memorizedD;
+    
+	private boolean isChosenDirectionFree() {
+		memorizedD = getDirection();
+		setDirection(chosenD);
+		if (!isFacingWall()) {
+			setDirection(memorizedD);
+			return true;
+		} else {
+			setDirection(memorizedD);
+			return false;
+    }
+	}
+    
+	public void keepHandOnSideWall() {
+		keepHandOnRightWall();
+	}
+	
+	private void keepHandOnRightWall() {
+		turnRight();
+		if (!isFacingWall()) {
+			angleSum = angleSum + 1;
+			forward(); // turn right then forward
+		} else {
+			turnLeft();
+			if (!isFacingWall()) {
+				forward(); // forward, direction did not change
+			} else {
+				turnLeft();
+				if (!isFacingWall()) {
+					angleSum = angleSum - 1;
+					forward(); // turn left then forward
+				} else {
+					angleSum = -2;
+					turnLeft(); // turn back then forward
+					forward();
+            }
+        }
+    }
+	}
+    
+# END TEMPLATE
