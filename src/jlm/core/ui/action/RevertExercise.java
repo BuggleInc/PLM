05e6e3f0eac5dda39110b0c07e94ed530a424219
Martin Quinson <martin.quinson@loria.fr>
2013-03-31 00:22:48 +0100

    Request a confirmation from the user before reverting the exercise
    (plus, cosmetics in the ChangeLog)

--- src/jlm/core/ui/action/RevertExercise.java
+++ src/jlm/core/ui/action/RevertExercise.java
@@ -3,6 +3,7 @@ package jlm.core.ui.action;
 import java.awt.event.ActionEvent;
 
 import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
 
 import jlm.core.model.Game;
 import jlm.core.model.ProgrammingLanguage;
@@ -21,10 +22,20 @@ public class RevertExercise extends AbstractGameAction {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		Lecture lect = game.getCurrentLesson().getCurrentExercise();
-		if (! (lect instanceof Exercise))
+		Object[] options = { "OK", "CANCEL" };
+		int choice = 
+			JOptionPane.showOptionDialog(null, "Reverting this exercise will erase all your work and cannot be undone.\n Are you sure that you want to proceed?", "Warning",
+				JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE,
+				null, options, options[0]);
+		if (choice != 0) {
+			System.out.println("Revert canceled on user request -- your work was preserved.");
 			return;
-		
+		}
+
+		Lecture lect = game.getCurrentLesson().getCurrentExercise();
+		if (! (lect instanceof Exercise)) 
+			return;
+
 		Exercise ex = (Exercise) lect;
 		for (ProgrammingLanguage lang: ex.getProgLanguages())
 			for (int i=0; i<ex.sourceFileCount(lang); i++) {
@@ -32,6 +43,7 @@ public class RevertExercise extends AbstractGameAction {
 				if (sf instanceof SourceFileRevertable)
 					((SourceFileRevertable) sf).revert();
 			}
+		System.out.println("Exercise reverted");
 	}
 
 }
