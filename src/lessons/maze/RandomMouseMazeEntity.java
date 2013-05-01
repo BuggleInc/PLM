9f243ab72a299303098cc8ab4532ca72b70833bf
Julien <julienbastian@yahoo.fr>
2013-05-01 20:53:01 +0200

    Simplify the random mouse maze

--- src/lessons/maze/RandomMouseMazeEntity.java
+++ src/lessons/maze/RandomMouseMazeEntity.java
@@ -19,76 +19,31 @@ public class RandomMouseMazeEntity extends jlm.universe.bugglequest.SimpleBuggle
 
 	/* BEGIN TEMPLATE */ 
 	public void run() {
-		while (!isOverBaggle()) {
-			if (atAJunction()) {
-				takeRandomDirection();
-			} else {
-				if (!isFacingWall()) {
-					forward();
-				} else {
-					turnRandomly();
-				}
+		// Your code here =)
+		/* BEGIN SOLUTION */ 
+		while (!isOverBaggle()) 
+		{
+			switch(random3()) 
+			{
+				case 0:
+					if (!isFacingWall())
+					{
+						forward();
+					}
+	                break;
+				case 1:
+					turnLeft();
+	                break;
+				case 2:
+	                turnRight();
+	                break;
 			}
 		}
 		pickUpBaggle();
+		/* END SOLUTION */
 	}
-
-	/* BEGIN SOLUTION */ 
-	public void turnRandomly() {
-		switch (random2()) {
-		case 0:
-			turnLeft();
-			break;
-		case 1:
-			turnRight();
-			break;
-		}
-	}
-
-	public void takeRandomDirection() {
-		if (isFacingWall()) {
-			turnRandomly();
-		} else {
-			switch (random3()) {
-			case 0:
-				turnLeft();
-				break;
-			case 1:
-				forward();
-				break;
-			case 2:
-				turnRight();
-				break;
-			}
-		}
-	}
-
-	public boolean atAJunction() {
-		boolean junction = false;
-
-		// check left
-		turnLeft();
-		if (!isFacingWall()) {
-			junction = true;
-		}
-		turnRight();
-
-		// can we skip next check?
-		if (junction) {
-			return true;
-		} else {
-			// check right
-			turnRight();
-			if (!isFacingWall()) {
-				junction = true;
-			}
-			turnLeft();
-		}
-		return junction;
-	}
-
 	/* END TEMPLATE */
-
+	
 	public int random3() {
 		double n = Math.random();
 		if (n < 0.33) {
@@ -97,15 +52,6 @@ public class RandomMouseMazeEntity extends jlm.universe.bugglequest.SimpleBuggle
 			return 1;
 		} else {
 			return 2;
-		}
-	}
-
-	public int random2() {
-		double n = Math.random();
-		if (n < 0.5) {
-			return 0;
-		} else {
-			return 1;
 		}
 	}
 
