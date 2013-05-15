d1078dc452d2e16500aa6b16d25fc858f30b7e5b
Martin Quinson <martin.quinson@loria.fr>
2013-05-15 14:37:42 +0200

    port 2 more sorting exos to the python templating

--- src/lessons/sort/insertion/AlgShellSortEntity.py
+++ src/lessons/sort/insertion/AlgShellSortEntity.py
@@ -1,27 +1,22 @@
 # BEGIN SOLUTION */
-		int gap = getValueCount()/2;
+gap = getValueCount()/2
 
-		/* while h remains larger than 0 */
-		while( gap > 0 ) {
-			/* for each set of elements (there are h sets) */
-			for (int i = gap - 1; i < getValueCount(); i++) {
-				/* pick the last element in the set */
-				int value = getValue(i);
-				int j = i;
-				/* compare the element at B to the one before it in the set
-				 * if they are out of order continue this loop, moving
-				 * elements "back" to make room for B to be inserted.
-				 */
-				for( j = i; (j >= gap) && (getValue(j-gap) > value); j -= gap) {
-					copy(j-gap,j);
-				}
-				/*  insert B into the correct place */
-				setValue(j, value);
-			}
-			/* all sets gap-sorted, now decrease set size */
-			gap = gap / 2;
-		}
-		for (int i = 0; i < getValueCount(); i++) 
-			sorted(i);
-		checkme();
-# END TEMPLATE */
+# while h remains larger than 0
+while gap>0:
+  # for each set of elements (there are h sets)
+  for i in range(gap-1,getValueCount()):
+    # pick the last element in the set
+    value = getValue(i)
+    j = i
+    # compare the element at B to the one before it in the set
+    # if they are out of order continue this loop, moving
+    # elements "back" to make room for B to be inserted.
+    while (j >= gap) and (getValue(j-gap) > value):
+      copy(j-gap,j)
+      j = j - gap
+    # insert B into the correct place
+    setValue(j, value)
+  # all sets gap-sorted, now decrease set size
+  gap = gap / 2
+# END TEMPLATE
+checkme()
