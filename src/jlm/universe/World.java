ea55f89854852bec954cbcb431055a0ac188cd9d
Martin Quinson <martin.quinson@loria.fr>
2013-05-16 22:54:30 +0200

    kill some more warnings

--- src/jlm/universe/World.java
+++ src/jlm/universe/World.java
@@ -7,7 +7,6 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
