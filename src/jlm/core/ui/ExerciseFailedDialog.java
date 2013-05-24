6b04cdad1cbb757b01c9e8793f2b33251683c433
Frank STENGEL <fstengel@mac.com>
2013-05-24 13:09:18 +0200

    Localization
    Some files. Last commit before refactoring of localization methods.

--- src/jlm/core/ui/ExerciseFailedDialog.java
+++ src/jlm/core/ui/ExerciseFailedDialog.java
@@ -11,14 +11,22 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.UIManager;
 
+import org.xnap.commons.i18n.I18n;
+import org.xnap.commons.i18n.I18nFactory;
+
 import jlm.core.model.lesson.ExecutionProgress;
 import net.miginfocom.swing.MigLayout;
 
 public class ExerciseFailedDialog extends JDialog {
 	private static final long serialVersionUID = 1L;
+	
+	public I18n i18n = I18nFactory.getI18n(getClass(),"org.jlm.i18n.Messages",getLocale(), I18nFactory.FALLBACK);
+
 
 	public ExerciseFailedDialog(ExecutionProgress ep) {
 		super(MainFrame.getInstance(), "Exercise failed /o\\", true);
+		
+		this.setTitle(i18n.tr("Exercise failed /o\\"));
 		
 		setLayout(new MigLayout("fill",""));
 		add(new JLabel( (Icon) UIManager.getLookAndFeelDefaults().get("OptionPane.errorIcon") ));
@@ -27,17 +35,17 @@ public class ExerciseFailedDialog extends JDialog {
 		JTextArea ta = new JTextArea();
 		ta.setEditable(false);
 		if (ep.compilationError == null) { 
-			msg = new JLabel( "You didn't manage to reach your objective." );
+			msg = new JLabel(i18n.tr("You didn't manage to reach your objective." ));
 			ta.setText(ep.details);
 		} else {
-			msg = new JLabel( "Compilation error" );
+			msg = new JLabel( i18n.tr("Compilation error") );
 			ta.setText(ep.compilationError);
 		}
 		ta.setCaretPosition(0);
 		add(msg,"wrap");
 		add(new JScrollPane(ta),"spanx, grow, growprio 200, wrap");
 		
-		JButton close = new JButton("Close");
+		JButton close = new JButton(i18n.tr("Close"));
 		close.addActionListener(new ActionListener() {
 			
 			@Override
