ce405d08dd15d731e46be57b87ee88f048b40ccb
Richard Guerci <richardg54@gmail.com>
2013-05-25 18:09:22 +0200

    Add an icon (when the jar is running, you can see a buggle on your dock) for Mac users only ;)

--- src/jlm/core/ui/JavaLearningMachine.java
+++ src/jlm/core/ui/JavaLearningMachine.java
@@ -1,6 +1,11 @@
 package jlm.core.ui;
 
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+
+import com.apple.eawt.Application;
 
 import jlm.core.model.Game;
 import jlm.core.model.FileUtils;
@@ -13,6 +18,10 @@ public class JavaLearningMachine {
 		if (System.getProperty("os.name").startsWith("Mac")) {
 			System.setProperty("apple.laf.useScreenMenuBar", "true");
 			System.setProperty("com.apple.mrj.application.apple.menu.about.name", "JLM");
+			//Dock Logo
+			Image img = ResourcesCache.getIcon("resources/logo.png").getImage(); 
+			Application app = Application.getApplication();
+			app.setDockIconImage(img);
 		}
 		
 		FileUtils.setLocale(new JFrame().getLocale().getLanguage());
