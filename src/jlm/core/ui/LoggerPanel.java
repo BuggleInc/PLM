740adec804f22ff2b2c606ae714346e54c2c36b3
Frank STENGEL <fstengel@mac.com>
2013-05-30 18:51:31 +0200

    Changes to LoggerPanel
    See diff.

--- src/jlm/core/ui/LoggerPanel.java
+++ src/jlm/core/ui/LoggerPanel.java
@@ -1,18 +1,21 @@
 package jlm.core.ui;
 
 import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.swing.JTextArea;
 import javax.tools.Diagnostic;
 import javax.tools.DiagnosticCollector;
 import javax.tools.JavaFileObject;
 
-import org.xnap.commons.i18n.I18n;
-import org.xnap.commons.i18n.I18nFactory;
 
 import jlm.core.HumanLangChangesListener;
 import jlm.core.model.Game;
 import jlm.core.model.LogWriter;
+
+import org.xnap.commons.i18n.I18n;
+import org.xnap.commons.i18n.I18nFactory;
 
 
 public class LoggerPanel extends JTextArea implements LogWriter, HumanLangChangesListener {
@@ -41,11 +44,24 @@ public class LoggerPanel extends JTextArea implements LogWriter, HumanLangChange
 
 	@Override
 	public void log(DiagnosticCollector<JavaFileObject> diagnostics) {
-		for (Diagnostic<? extends JavaFileObject> diagnostic : diagnostics.getDiagnostics()) {			
-			append(diagnostic.getSource().getName()+":"+diagnostic.getLineNumber()+":"+ diagnostic.getMessage(getLocale()));
-			append("\n");
+		boolean warnedJava6= false;
+		Pattern isJava6Pattern = Pattern.compile("major version 51 is newer than 50, the highest major version supported by this compiler");
+
+		for (Diagnostic<? extends JavaFileObject> diagnostic : diagnostics.getDiagnostics()) {
+			String source = diagnostic.getSource() == null ? "(null)" : diagnostic.getSource().getName();
+			String msg = diagnostic.getMessage(getLocale());
+
+			Matcher isJava6Matcher = isJava6Pattern.matcher(msg);
+			if (isJava6Matcher.find()) {
+				if (!warnedJava6 && Game.getInstance().isDebugEnabled())
+					append("You are using a JLM jarfile that was compiled for Java 6, but you have a Java 7 runtime. This is *believed* to work.\n");
+				warnedJava6 = true;
+			} else {
+				append(source+":"+diagnostic.getLineNumber()+":"+ msg+"\n");
+			}
 		}
 	}
+
 
 	/**
 	 * Add an exception into the text area
