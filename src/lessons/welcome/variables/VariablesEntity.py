c2fff65d789c2a9794e1b9d391d3e95ca0e37d78
Martin Quinson <martin.quinson@loria.fr>
2013-06-01 21:34:35 +0200

    https://www.youtube.com/watch?v=ARJ8cAGm6JE

--- src/lessons/welcome/variables/VariablesEntity.py
+++ src/lessons/welcome/variables/VariablesEntity.py
@@ -1,3 +1,11 @@
+def forward(i):
+    if i>1:
+        errorMsg("Sorry Dave, I cannot let you use forward(i>1) in this exercise")
+
+def backward(i):
+    if i>1:
+        errorMsg("Sorry Dave, I cannot let you use backward(i>1) in this exercise")
+
 # BEGIN SOLUTION 
 cpt = 0
 while isOverBaggle() == False:
