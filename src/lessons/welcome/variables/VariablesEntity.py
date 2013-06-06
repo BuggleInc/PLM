86758ea9482637651138a3e3fb9b8b0a77ac8113
Martin Quinson <martin.quinson@loria.fr>
2013-06-06 09:25:44 +0200

    pythonizes; anti-cheating protections should not prevent to pass the exercises

--- src/lessons/welcome/variables/VariablesEntity.py
+++ src/lessons/welcome/variables/VariablesEntity.py
@@ -1,19 +1,20 @@
 def forward(i=1):
     if i>1:
         errorMsg("Sorry Dave, I cannot let you use forward(i>1) in this exercise")
-
+    entity.forward()
 def backward(i=1):
     if i>1:
         errorMsg("Sorry Dave, I cannot let you use backward(i>1) in this exercise")
+    entity.backward()
 
 # BEGIN SOLUTION 
 cpt = 0
-while isOverBaggle() == False:
-  cpt = cpt +1
+while not isOverBaggle():
+  cpt += 1 
   forward()
 pickUpBaggle()
 while cpt>0:
   backward()
-  cpt = cpt-1
+  cpt -= 1
 dropBaggle()
 # END SOLUTION
