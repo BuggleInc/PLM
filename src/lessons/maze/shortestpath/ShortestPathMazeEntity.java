6e25ccb0e41360c73ee345ec5f3d831c6c5a839d
Martin Quinson <martin.quinson@loria.fr>
2013-06-28 22:32:32 +0200

    Simplify a bit the last maze exercise to ease its pythonization

--- src/lessons/maze/shortestpath/ShortestPathMazeEntity.java
+++ src/lessons/maze/shortestpath/ShortestPathMazeEntity.java
@@ -24,93 +24,74 @@ public class ShortestPathMazeEntity extends jlm.universe.bugglequest.SimpleBuggl
 		super.setPos(i,j);
 	}
 
-	public BuggleWorld getMyWorld() {
-		return (BuggleWorld) getWorld();
+	void setIndication(int x, int y, int i) {
+		BuggleWorldCell c = ((BuggleWorld) world).getCell(x,y);
+		c.setContent(""+i);
+	}
+	int getIndication(int x, int y) {
+		BuggleWorldCell c = ((BuggleWorld) world).getCell(x,y);
+		if (c.hasContent())
+			return Integer.parseInt(c.getContent());
+		return 9999;
+	}
+	boolean hasBaggle(int x, int y) {
+		return ((BuggleWorld) world).getCell(x,y).hasBaggle();
+	}
+	boolean hasTopWall(int x, int y) {
+		return ((BuggleWorld) world).getCell(x,y).hasTopWall();
+	}
+	boolean hasLeftWall(int x, int y) {
+		return ((BuggleWorld) world).getCell(x,y).hasLeftWall();
 	}
 
 	/* BEGIN TEMPLATE */
 	/* BEGIN SOLUTION */
 	// tools functions
-	public BuggleWorldCell bottomCell(int x, int y) {
-		return getMyWorld().getCell(x, (y + 1) % getWorldHeight());
-	}
-
-	public BuggleWorldCell rightCell(int x, int y) {
-		return getMyWorld().getCell((x + 1) % getWorldWidth(), y); 
-	}  
-
-	public BuggleWorldCell topCell(int x, int y) {
-		return getMyWorld().getCell(x, ( y + getWorldHeight() - 1) % getWorldHeight());
-	}
-
-	public BuggleWorldCell leftCell(int x, int y) {
-		return getMyWorld().getCell((x + getWorldWidth() - 1) % getWorldWidth(), y); 
-	}  
-
-	public boolean hasTopWall(int x, int y) {
-		return getMyWorld().getCell(x, y).hasTopWall();
-	}
-
-	public boolean hasLeftWall(int x, int y) {
-		return getMyWorld().getCell(x, y).hasLeftWall(); 
-	}  
-
 	public boolean hasRightWall(int x, int y) {
-		return getMyWorld().getCell((x + 1) % getWorldWidth(), y).hasLeftWall(); 
+		return hasLeftWall((x + 1) % getWorldWidth(), y); 
 	}  
 
 	public boolean hasBottomWall(int x, int y) {
-		return getMyWorld().getCell(x, (y + 1) % getWorldHeight()).hasTopWall();
+		return hasTopWall(x, (y + 1) % getWorldHeight());
 	}
 
 
 	public boolean setValueIfLess(int x, int y, int val) {
-		BuggleWorldCell cell = getMyWorld().getCell(x,y); 
-
-		if (! cell.hasContent()) {
-			cell.setContentFromInt(val);
+		int existing = getIndication(x, y);
+		if (val < existing) {
+			setIndication(x, y, val);
 			return true;
-		} else {
-			int currentVal = cell.getContentAsInt();
-			if (val < currentVal) {
-				cell.setContentFromInt(val); 
-				return true;  
-			}
 		}
-
 		return false;
 	}
 
 	public void evaluatePaths() {
 		// looking for labyrinth exit	
 		for (int x = 0; x < getWorldWidth(); x++)
-			for (int y = 0; y < getWorldHeight(); y++) {       
-				BuggleWorldCell c = getMyWorld().getCell(x,y);
-				if (c.hasBaggle())
-					c.setContentFromInt(0);   
-			}
+			for (int y = 0; y < getWorldHeight(); y++)        
+				if (hasBaggle(x,y))
+					setIndication(x, y, 0);
 
 		while (true) {   
 			for (int x = 0; x < getWorldWidth(); x++) {
 				for (int y = 0; y < getWorldHeight(); y++) {  
-					BuggleWorldCell c = getMyWorld().getCell(x,y); 
+					int indication = getIndication(x, y);
 					boolean changed = false;
-					if (c.hasContent()) {
-						int val = c.getContentAsInt();
+					if (indication != 9999) {
 						if (! hasBottomWall(x,y))
-							changed |= setValueIfLess(x, (y + 1) % getWorldHeight(), val + 1);
+							changed |= setValueIfLess(x, (y + 1) % getWorldHeight(), indication + 1);
 
 						if (! hasRightWall(x,y))
-							changed |= setValueIfLess((x + 1) % getWorldWidth(), y, val + 1);
+							changed |= setValueIfLess((x + 1) % getWorldWidth(), y, indication + 1);
 
 						if (! hasTopWall(x,y))
-							changed |= setValueIfLess(x, (y+getWorldHeight() - 1) % getWorldHeight(), val + 1);
+							changed |= setValueIfLess(x, (y+getWorldHeight() - 1) % getWorldHeight(), indication + 1);
 
 						if (! hasLeftWall(x,y))
-							changed |= setValueIfLess((x +getWorldWidth() - 1) % getWorldWidth(), y, val + 1);
+							changed |= setValueIfLess((x +getWorldWidth() - 1) % getWorldWidth(), y, indication + 1);
 
 						if (changed && x == getX() && y == getY())
-							return ;
+							return ; // reached the buggle, that's enough
 					}
 				}    
 			}
@@ -120,36 +101,36 @@ public class ShortestPathMazeEntity extends jlm.universe.bugglequest.SimpleBuggl
 	public void followShortestPath() {
 		while (! isOverBaggle()) {
 
-			int zx = getX();
-			int zy = getY();
+			int x = getX();
+			int y = getY();
 
-			int topValue = Integer.MAX_VALUE;
-			int bottomValue = Integer.MAX_VALUE;
-			int leftValue = Integer.MAX_VALUE;
-			int rightValue = Integer.MAX_VALUE;
+			int topValue = 9999;
+			int bottomValue = 9999;
+			int leftValue = 9999;
+			int rightValue = 9999;
 
-			if (! hasTopWall(zx, zy))
-				topValue = topCell(zx,zy).hasContent()?topCell(zx,zy).getContentAsInt():Integer.MAX_VALUE;
+			if (! hasTopWall(x, y))
+				topValue = getIndication(x, (y + getWorldHeight() - 1) % getWorldHeight());
 
-				if (! hasBottomWall(zx, zy))
-					bottomValue = bottomCell(zx,zy).hasContent()?bottomCell(zx,zy).getContentAsInt():Integer.MAX_VALUE;
+			if (! hasBottomWall(x, y))
+				bottomValue = getIndication(x, (y+1) % getWorldHeight());
 
-					if (! hasLeftWall(zx, zy))
-						leftValue = leftCell(zx,zy).hasContent()?leftCell(zx,zy).getContentAsInt():Integer.MAX_VALUE;
+			if (! hasLeftWall(x, y))
+				leftValue = getIndication((x +getWorldWidth() - 1) % getWorldWidth(), y);
 
-						if (! hasRightWall(zx, zy))
-							rightValue = rightCell(zx,zy).hasContent()?rightCell(zx,zy).getContentAsInt():Integer.MAX_VALUE;
+			if (! hasRightWall(x, y))
+				rightValue = getIndication((x + 1) % getWorldWidth(), y);
+			
+			if (topValue <= bottomValue && topValue <= leftValue && topValue <= rightValue)
+				setDirection(Direction.NORTH);
+			else if (rightValue <= topValue && rightValue <= bottomValue && rightValue <= leftValue)
+				setDirection(Direction.EAST);
+			else if (leftValue <= rightValue && leftValue <= bottomValue && leftValue <= topValue)
+				setDirection(Direction.WEST);
+			else if (bottomValue <= topValue && bottomValue <= rightValue && bottomValue <= leftValue)
+				setDirection(Direction.SOUTH);
 
-							if (topValue <= bottomValue && topValue <= leftValue && topValue <= rightValue)
-								setDirection(Direction.NORTH);
-							else if (rightValue <= topValue && rightValue <= bottomValue && rightValue <= leftValue)
-								setDirection(Direction.EAST);
-							else if (leftValue <= rightValue && leftValue <= bottomValue && leftValue <= topValue)
-								setDirection(Direction.WEST);
-							else if (bottomValue <= topValue && bottomValue <= rightValue && bottomValue <= leftValue)
-								setDirection(Direction.SOUTH);
-
-							forward();
+			forward();
 		}    
 	}
 	/* END SOLUTION */
