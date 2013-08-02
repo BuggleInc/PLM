5c9ff7f08cd0ad6ead37f2384e80aa35b83ff719
Martin Quinson <martin.quinson@loria.fr>
2013-08-02 22:09:18 +0200

    add a world with the hole in the first positino to make sure that the students don't miss that case

--- src/lessons/sort/baseball/InsertBaseball.java
+++ src/lessons/sort/baseball/InsertBaseball.java
@@ -16,7 +16,7 @@ public class InsertBaseball extends ExerciseTemplated {
 				new BaseballWorld("7 bases",7,2),
 				new BaseballWorld("8 bases",8,2),
 				new BaseballWorld("9 bases",9,2),
-				new BaseballWorld("10 bases",10,2),
+				new BaseballWorld("10 bases",10,2,new int[]{-1,7 , 2,7 , 1,4 , 3,4 , 5,6 , 8,9 , 5,1 , 3,6 , 0,2 , 0,8}),
 				new BaseballWorld("15 bases",15,2),
 		});
 	}
