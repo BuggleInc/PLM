5d1a3d80362b294c726d1706c22342565734152c
Martin Quinson <martin.quinson@loria.fr>
2013-08-08 01:11:33 +0200

    scalaize the very first exercise of the track

---
+++ src/lessons/welcome/environment/EnvironmentEntity.scala
@@ -0,0 +1,11 @@
+package lessons.welcome.environment;
+
+import jlm.universe.bugglequest.SimpleBuggle;
+
+class ScalaEnvironmentEntity extends SimpleBuggle {
+	protected def run() { 
+		/* BEGIN SOLUTION */
+		forward();
+		/* END SOLUTION */
+	}
+}
