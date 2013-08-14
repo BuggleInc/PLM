d2196b5feb28db467db6fc44a3847a0dd6fbed77
Martin Quinson <martin.quinson@loria.fr>
2013-08-15 01:55:02 +0200

    switch debug to true in the mission editor to see the tags with a class

--- src/jlm/core/ui/editor/MissionEditorApp.java
+++ src/jlm/core/ui/editor/MissionEditorApp.java
@@ -1,15 +1,14 @@
 package jlm.core.ui.editor;
 
-
+import jlm.core.model.Game;
 
 public class MissionEditorApp {
 
 	public static void main(String[] args) {
 		MissionEditor editor = new MissionEditor();
+		Game.getInstance().switchDebug(); // Forces the JlmHTMLEditorKit to also display all blocks marked with a class
 		if (args.length>0)
 			editor.loadMission(args[0]);
-		else
-			editor.loadMission("/home/mquinson/Code/JLM/src/lessons/welcome/loopfor/LoopFor.html");
 	}
 
 }
