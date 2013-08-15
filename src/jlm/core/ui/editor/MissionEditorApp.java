4baee2ca8b6a7b51a116f9a011d53bc5c512f87f
Martin Quinson <martin.quinson@loria.fr>
2013-08-15 09:21:07 +0200

    try to open a file of the project by default so that the lastSelectedPath is correctly filed

--- src/jlm/core/ui/editor/MissionEditorApp.java
+++ src/jlm/core/ui/editor/MissionEditorApp.java
@@ -1,5 +1,7 @@
 package jlm.core.ui.editor;
 
+import java.io.File;
+
 import jlm.core.model.Game;
 
 public class MissionEditorApp {
@@ -9,6 +11,12 @@ public class MissionEditorApp {
 		Game.getInstance().switchDebug(); // Forces the JlmHTMLEditorKit to also display all blocks marked with a class
 		if (args.length>0)
 			editor.loadMission(args[0]);
+		else {
+			String path = System.getProperty("user.dir")+"/src/lessons/welcome/Main.html";
+			File f = new File(path);
+			if (f.exists())
+				editor.loadMission(path);
+		}
 	}
 
 }
