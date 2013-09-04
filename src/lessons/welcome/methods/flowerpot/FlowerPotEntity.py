a6a45e79673ff55953a8de0284ac7fd7d1eb94c8
Martin Quinson <martin.quinson@loria.fr>
2013-09-04 21:13:26 +0200

    cosmetics: reindent that code

--- src/lessons/welcome/methods/flowerpot/FlowerPotEntity.py
+++ src/lessons/welcome/methods/flowerpot/FlowerPotEntity.py
@@ -13,26 +13,26 @@ def makeFlower(c):
 	right()   
 
 def line(c1, c2):
-	    makeFlower(c1);
-	    forward(3);
-	    makeFlower(c2);
-	    backward(5);    
+    makeFlower(c1)
+    forward(3)
+    makeFlower(c2)
+    backward(5)    
 def halfLine(c):
-	    forward(2);
-	    makeFlower(c);
-	    backward(3);
+    forward(2)
+    makeFlower(c)
+    backward(3)
 	
 def growFlowers():
-	    line(Color.RED, Color.CYAN);
+    line(Color.RED, Color.CYAN)
 	    
-	    right();    
-	    forward(2);
-	    left();
-	    halfLine(Color.ORANGE);
-	    right();
-	    forward(2);
-	    left();
+    right()    
+    forward(2)
+    left()
+    halfLine(Color.ORANGE)
+    right()
+    forward(2)
+    left()
 	    
-	    line(Color.PINK, Color.GREEN);
+    line(Color.PINK, Color.GREEN);
 # END SOLUTION
 growFlowers()
