77055616ebb537714ac6da9d97d015e3829ace7b
Martin Quinson <martin.quinson@loria.fr>
2013-09-12 21:29:16 +0200

    make DontHaveBaggleException extends PLMException, that is RE to not have to declare its possible occurence

--- src/plm/universe/bugglequest/exception/DontHaveBaggleException.java
+++ src/plm/universe/bugglequest/exception/DontHaveBaggleException.java
@@ -1,8 +1,9 @@
 package plm.universe.bugglequest.exception;
 
+import plm.core.PLMException;
 import plm.core.model.Game;
 
-public class DontHaveBaggleException extends Exception {
+public class DontHaveBaggleException extends PLMException {
 	private static final long serialVersionUID = 1L;
 
 	public DontHaveBaggleException() {
