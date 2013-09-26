9f164265992cb85f2675fa6247bb689e849212c6
Martin Quinson <martin.quinson@loria.fr>
2013-09-26 21:27:10 +0200

    recursion.hanoi: Simplify by explicitely passing the index of the unused peg as a parameter

--- src/lessons/recursion/hanoi/HanoiBoardEntity.java
+++ src/lessons/recursion/hanoi/HanoiBoardEntity.java
@@ -5,28 +5,20 @@ import lessons.recursion.hanoi.universe.HanoiEntity;
 public class HanoiBoardEntity extends HanoiEntity {
 
 	public void run() {
-		solve((Integer)getParam(0),(Integer) getParam(1));
+		solve((Integer)getParam(0),(Integer) getParam(1),(Integer) getParam(2));
 	}
 
 	/* BEGIN TEMPLATE */
-	public void solve(int src,int dst) {
-		solve(src,dst, getSlotSize(src));
+	public void solve(int src, int dst, int other) {
+		solve(src, dst, other, getSlotSize(src));
 	}
 
-	public void solve(int src, int dst, int height) {
+	public void solve(int src, int dst, int other, int height) {
 		/* BEGIN SOLUTION */
-		if (height!=0) {
-			int other=-1;
-			if (src+dst==1) /* 0+1 */
-				other=2;
-			if (src+dst==2) /* 0+2 */
-				other=1;
-			if (src+dst==3) /* 1+2 */
-				other=0;
-
-			solve(src,other, height-1);
+		if (height != 0) {
+			solve(src,other,dst, height-1);
 			move(src,dst);
-			solve(other,dst,height-1);
+			solve(other,dst,src, height-1);
 		}
 		/* END SOLUTION */
 	}
--- src/lessons/recursion/hanoi/HanoiBoardEntity.py
+++ src/lessons/recursion/hanoi/HanoiBoardEntity.py
@@ -1,21 +1,14 @@
 # BEGIN TEMPLATE
-def solve(src,dst,height=-1):
+def solve(src,dst,other, height=-1):
 	if height==-1: # initial case
 		height=getSlotSize(src) 
 	
 	# Your code here
 	# BEGIN SOLUTION
 	if height != 0:
-		if src+dst==1:   # 0+1
-			other=2
-		if src+dst==2:   # 0+2
-			other=1;
-		if src+dst==3:   # 1+2
-			other=0;
-
-		solve(src,other, height-1);
+		solve(src,other,dst, height-1);
 		move(src,dst);
-		solve(other,dst,height-1);
+		solve(other,dst,src, height-1);
 	# END SOLUTION
 # END TEMPLATE
-solve(getParam(0),getParam(1))
+solve(getParam(0),getParam(1),getParam(2))
