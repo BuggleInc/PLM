0c2ed915a65f22d7192a9a53b91aeabcf031279f
Martin Quinson <martin.quinson@loria.fr>
2013-09-30 21:53:37 +0200

    recursion.SquareKoch: new exercise (variation over the Koch idea)

--- src/lessons/recursion/Main.java
+++ src/lessons/recursion/Main.java
@@ -3,6 +3,7 @@ package lessons.recursion;
 import lessons.recursion.dragoncurve.DragonCurve1;
 import lessons.recursion.dragoncurve.DragonCurve2;
 import lessons.recursion.koch.Koch;
+import lessons.recursion.koch.SquareKoch;
 import lessons.recursion.polygonfractal.PolygonFractal;
 import lessons.recursion.sierpinski.Sierpinski;
 import lessons.recursion.spiral.Spiral;
@@ -19,6 +20,7 @@ public class Main extends Lesson {
 		addExercise(new SpiralUse(this));
 		addExercise(new Tree(this));
 		addExercise(new Koch(this));
+		addExercise(new SquareKoch(this));
 		addExercise(new Sierpinski(this));
 		addExercise(new PolygonFractal(this));
 		addExercise(new DragonCurve1(this));
