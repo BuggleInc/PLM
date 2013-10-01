caf513b17c8b608e9b7d9ee3326030f60df46373
Martin Quinson <martin.quinson@loria.fr>
2013-10-01 11:11:37 +0200

    graphical hint to the square Koch curve

--- src/lessons/recursion/koch/SquareKochEntity.java
+++ src/lessons/recursion/koch/SquareKochEntity.java
@@ -38,6 +38,22 @@ public class SquareKochEntity extends Turtle {
 		/* END SOLUTION */	
 	}
 	/* END TEMPLATE */
+	public void drawHint() {
+	    clear();
+	    hide();
+	    setColor(Color.red);
+	    setPos(100,50);setHeading(0);
+	    squareSide(0,200);
+	    
+	    setPos(100,150); setHeading(0);
+	    squareSide(1,200);
+
+	    setPos(100,250); setHeading(0);
+	    squareSide(2,200);
+	    
+	    setPos(100,350); setHeading(0);
+	    squareSide(3,200);	    
+	}
 
 	public void run() {
 		snowSquare((Integer)getParam(0),(Double)getParam(1));
