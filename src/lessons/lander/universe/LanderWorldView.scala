67dfe57d4eebeb73a380c0664254bb408687c883
Paul Brauner <polux@google.com>
2014-02-27 23:02:02 +0100

    cleanup drawing

--- src/lessons/lander/universe/LanderWorldView.scala
+++ src/lessons/lander/universe/LanderWorldView.scala
@@ -15,37 +15,52 @@ class LanderWorldView(world: LanderWorld) extends WorldView(world.parent) {
 
   override def paintComponent(g: Graphics) = {
     super.paintComponent(g);
-
     val g2 = g.asInstanceOf[Graphics2D];
-    clearWidgetBackground(g2)
-    setupGlobalTransformation(g2)
-    clearWorldBackground(g2)
-    drawGround(g2)
+    new Painter(g2).paint()
   }
 
-  def clearWidgetBackground(g2: Graphics2D) = {
-    g2.setColor(Color.gray)
-    g2.fill(new Rectangle2D.Double(0, 0, getWidth, getHeight))
-  }
+  class Painter(g2: Graphics2D) {
+    def paint() {
+      setupRendering()
+      clearWidgetBackground()
+      setupGlobalTransform()
+      clearWorldBackground()
+      drawGround()
+    }
 
-  def setupGlobalTransformation(g2: Graphics2D) = {
-    val scale = Math.min(getWidth.toDouble / world.width, getHeight.toDouble / world.height)
-    g2.translate((getWidth - world.width * scale) / 2,  (getHeight + world.height * scale) / 2)
-    g2.scale(scale, -scale)
-    g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-    g2.setStroke(new BasicStroke((1 / scale).toFloat))
-  }
+    def setupRendering() {
+      g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
+    }
 
-  def clearWorldBackground(g2: Graphics2D) = {
-    g2.setColor(Color.black);
-    g2.fill(new Rectangle2D.Double(0, 0, world.width, world.height));
-  }
+    def clearWidgetBackground() = {
+      g2.setColor(Color.gray)
+      g2.fill(new Rectangle2D.Double(0, 0, getWidth, getHeight))
+    }
 
-  def drawGround(g2: Graphics2D) = {
-    if (world.ground.length > 1) {
-      for ((p1, p2) <- world.ground zip world.ground.tail) {
-        g2.setColor(Color.white);
-        g2.draw(new Line2D.Double(p1.x, p1.y, p2.x, p2.y))
+    def setupGlobalTransform() = {
+      val scale = (getWidth.toDouble / world.width) min (getHeight.toDouble / world.height)
+      g2.translate((getWidth - world.width * scale) / 2,  (getHeight + world.height * scale) / 2)
+      g2.scale(scale, -scale)
+    }
+
+    def setVisibleLineWidthTo1px() {
+      val transform = g2.getTransform();
+      val scale = (transform.getScaleX() min transform.getScaleY()).abs;
+      g2.setStroke(new BasicStroke((1 / scale).toFloat))
+    }
+
+    def clearWorldBackground() = {
+      g2.setColor(Color.black);
+      g2.fill(new Rectangle2D.Double(0, 0, world.width, world.height));
+    }
+
+    def drawGround() = {
+      setVisibleLineWidthTo1px()
+      if (world.ground.length > 1) {
+        for ((p1, p2) <- world.ground zip world.ground.tail) {
+          g2.setColor(Color.white);
+          g2.draw(new Line2D.Double(p1.x, p1.y, p2.x, p2.y))
+        }
       }
     }
   }
