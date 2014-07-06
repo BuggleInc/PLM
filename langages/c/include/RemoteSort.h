8e489a0aa69a3f9241b025c60a666d8cc5f3b3f1
Giannini Valentin <valentin.giannini@telecomnancy.net>
2014-07-06 20:41:33 +0200

    add RemoteSort for C

---
+++ langages/c/include/RemoteSort.h
@@ -0,0 +1,31 @@
+#ifndef REMOTE_SORT_H
+#define REMOTE_SORT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+void flush();
+
+int getValueCount();
+int isSmaller(int i, int j);
+int isSmallerThan(int i, int value);
+void swap(int i, int j);
+void copy(int i, int j);
+int getValue(int idx);
+void setValue(int idx, int value);
+int isSelected();
+
+
+/* BINDINGS TRANSLATION: French */
+int getNombreValeurs(); 				
+int getValeur(int i);   				
+void setValeur(int i,int val); 		
+int plusPetit(int i, int j); 			
+int plusPetitQue(int i, int value);	
+void echange(int i, int j);			
+void copie(int from,int to); 		
+
+
+#endif
