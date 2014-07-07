5a4d5082a3666dce508930d8f0cf37184fa04869
Giannini Valentin <valentin.giannini@telecomnancy.net>
2014-07-07 10:09:58 +0200

    add RemoteHanoi

---
+++ langages/c/include/RemoteHanoi.h
@@ -0,0 +1,22 @@
+#ifndef REMOTE_HANOI_H
+#define REMOTE_HANOI_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+void flush();
+
+void move(int src, int dst);
+int getSlotSize(int slot);
+int isSelected();
+
+int getParam(int nb);
+
+/* BINDINGS TRANSLATION: French */
+void deplace(int src,int dst);
+int  getTaillePiquet(int rank);
+int estChoisi();
+
+#endif
---
+++ langages/c/src/RemoteHanoi.c
@@ -0,0 +1,48 @@
+#include "RemoteHanoi.h"
+
+void flush(){
+    int c = 0;
+    FILE* f = fopen("/tmp/flush","a+");
+    while (c != '\n' && c != EOF){
+        c = getchar();
+        fputc(c,f);
+    }
+    fclose(f);
+}
+
+void move(int src, int dst){
+	printf("110 %d %d move\n", src, dst);
+	fflush(stdout);
+}
+
+int getSlotSize(int slot){
+	int size;
+	printf("111 %d getSlotSize\n", slot);
+	fflush(stdout);
+	scanf("%d",&size);
+	flush();
+	return size;
+}
+
+int isSelected(){
+	int sel;
+	printf("112 isSelected\n");
+	fflush(stdout);
+	scanf("%d", &sel);
+	flush();
+	return sel;
+}
+
+int getParam(int nb){
+	int param;
+	printf("114 %d getParam\n", nb);
+	fflush(stdout);
+	scanf("%d",&param);
+	flush();
+	return param;
+}
+
+/* BINDINGS TRANSLATION: French */
+void deplace(int src,int dst) 	{ move(src, dst); }
+int  getTaillePiquet(int rank) 	{ return getSlotSize(rank); }
+int estChoisi() 				{ return isSelected(); }
