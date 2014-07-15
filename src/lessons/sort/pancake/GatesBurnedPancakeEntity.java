1026a9cbef6d0b717a0fb65f1513c09e5743306e
Giannini Valentin <valentin.giannini@telecomnancy.net>
2014-07-15 11:36:54 +0200

    add files for gatesBurned pancake

---
+++ src/lessons/sort/pancake/GatesBurnedPancakeEntity.java
@@ -0,0 +1,20 @@
+package lessons.sort.pancake;
+
+import lessons.sort.pancake.universe.PancakeEntity;
+
+public class GatesBurnedPancakeEntity extends PancakeEntity {
+
+
+	public void run() {
+		this.solve();
+	}
+
+	/* BEGIN TEMPLATE */
+	public void solve() {
+		/* BEGIN SOLUTION */
+		
+		/* END SOLUTION */
+	}
+	/* END TEMPLATE */
+
+}
