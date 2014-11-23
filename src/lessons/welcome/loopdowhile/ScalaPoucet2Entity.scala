6985664285f256afb9f1bb69128f56b62d582b81
Martin Quinson <martin.quinson@loria.fr>
2014-11-23 21:30:04 +0100

    fix a stupid typo

--- src/lessons/welcome/loopdowhile/ScalaPoucet2Entity.scala
+++ src/lessons/welcome/loopdowhile/ScalaPoucet2Entity.scala
@@ -5,7 +5,7 @@ import plm.core.model.Game
 import plm.universe.GridWorld
 import plm.universe.bugglequest.BuggleWorldCell
 
-class ScalaPoucetEntity extends plm.universe.bugglequest.SimpleBuggle {
+class ScalaPoucet2Entity extends plm.universe.bugglequest.SimpleBuggle {
 	override def forward(i: Int)  { 
 		throw new RuntimeException(Game.i18n.tr("Sorry Dave, I cannot let you use forward with an argument in this exercise. Use a loop instead."));
 	}
