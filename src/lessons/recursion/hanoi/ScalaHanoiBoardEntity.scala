423b18c42cc945c0c371187f9de8cd54402348ec
Martin Quinson <martin.quinson@loria.fr>
2014-12-30 00:29:43 +0100

    InterleavedHanoi: new variation over the Hanoi problem

---
+++ src/lessons/recursion/hanoi/ScalaHanoiBoardEntity.scala
@@ -0,0 +1,27 @@
+package lessons.recursion.hanoi;
+
+import lessons.recursion.hanoi.universe.HanoiEntity;
+
+class ScalaHanoiBoardEntity extends HanoiEntity {
+
+	override def run() {
+		solve(getParam(0).asInstanceOf[Int], getParam(1).asInstanceOf[Int], getParam(2).asInstanceOf[Int]);
+	}
+
+	/* BEGIN TEMPLATE */
+	def solve(src:Int, other:Int, dst:Int) {
+		hanoi(getSlotSize(src), src,other,dst);
+	}
+
+	def hanoi(height:Int, src:Int, other:Int, dst:Int) {
+		/* BEGIN SOLUTION */
+		if (height!=0) {
+			hanoi(height-1,  src,dst,other);
+			move(src,dst);
+			hanoi(height-1,  other,src,dst);
+		}
+		/* END SOLUTION */
+	}
+	/* END TEMPLATE */
+
+}
