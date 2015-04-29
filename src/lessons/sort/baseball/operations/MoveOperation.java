200e3bc82825aa4b747dac6e899425970d6caf74
Théodore lambolez <theodore.lambolez@gmail.com>
2015-04-29 15:51:10 +0200

    add oldBase and oldposition in moveoperation.java

--- src/lessons/sort/baseball/operations/MoveOperation.java
+++ src/lessons/sort/baseball/operations/MoveOperation.java
@@ -6,6 +6,9 @@ public class MoveOperation extends BaseballOperation {
 
 	private int base;
 	private int position;
+	private int oldBase;
+	private int oldPosition;
+
 	
 	public MoveOperation(BaseballEntity entity, int base, int position) {
 		super("moveOperation", entity);
@@ -13,6 +16,15 @@ public class MoveOperation extends BaseballOperation {
 		this.position = position;
 	}
 	
+
+	public int getOldBase() {
+		return oldBase;
+	}
+
+	public int getOldPosition() {
+		return oldPosition;
+	}
+
 	public int getPosition()
 	{
 		return position;
