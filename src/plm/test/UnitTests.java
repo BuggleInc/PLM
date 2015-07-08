052e99760bb834c971e7cfdf89e880e5d880ad35
Tanguy Gloaguen <tanguy.gloaguen@telecomnancy.net>
2015-07-08 15:02:14 +0200

    Woops. Removed unit tests.

--- src/plm/test/UnitTests.java
+++ src/plm/test/UnitTests.java
@@ -4,11 +4,9 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import plm.test.git.GitSpyTest;
-import plm.test.git.GitUtilsTest;
 import plm.test.simple.test.AllSimpleExerciseTests;
 
 @RunWith(Suite.class)
-@SuiteClasses({ AllSimpleExerciseTests.class, GitSpyTest.class, GitUtilsTest.class })
+@SuiteClasses({ AllSimpleExerciseTests.class})
 public class UnitTests {
 }
