717f4d7135c897ef3158fc449e583411894ac310
AlexandreCarpentier <alexandre.carpentier14@gmail.com>
2015-07-24 14:51:15 +0200

    Corrected some small errors and added new common errors in the Variables exercise

--- src/lessons/welcome/variables/VariablesCommonErr3.java
+++ src/lessons/welcome/variables/VariablesCommonErr3.java
@@ -2,7 +2,7 @@ package lessons.welcome.variables;
 
 import plm.core.model.Game;
 
-public class VariablesCommonErr0 extends plm.universe.bugglequest.SimpleBuggle {
+public class VariablesCommonErr3 extends plm.universe.bugglequest.SimpleBuggle {
 	@Override
 	public void forward(int i)  { 
 		throw new RuntimeException(Game.i18n.tr("Sorry Dave, I cannot let you use forward with an argument in this exercise. Use a loop instead."));
@@ -16,27 +16,7 @@ public class VariablesCommonErr0 extends plm.universe.bugglequest.SimpleBuggle {
 
 	@Override
 	public void run() {
-		int nbPas = 0;
-		int reculePas = 0;
-			
-		while(!estSurBiscuit())
-		{
-			nbPas = 0;
-			reculePas = 0;
-
-			avance();
-			nbPas++;
-
-			if(estSurBiscuit())
-			{		
-				prendBiscuit();
-				while(reculePas != nbPas)
-				{
-					recule();
-					reculePas++;
-				}
-				poseBiscuit();
-			}
-		}
+		while(!isOverBaggle())
+			forward();
 	}
 }
