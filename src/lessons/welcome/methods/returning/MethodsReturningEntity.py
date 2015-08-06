76f243e7a12ccdb9e2594f079d72de1212c24000
Matthieu Nicolas <nicolasmatthieu57@gmail.com>
2015-08-06 16:53:44 +0200

    Merge branch 'master' into library

--- src/lessons/welcome/methods/returning/MethodsReturningEntity.py
+++ src/lessons/welcome/methods/returning/MethodsReturningEntity.py
@@ -1,11 +1,11 @@
-def forward(i=-1):
-    if i!=-1:
+def forward(i=1):
+    if i>1:
         errorMsg("Sorry Dave, I cannot let you use forward with an argument in this exercise. Use a loop instead.")
     else:
         entity.forward()
 
-def backward(i=-1):
-    if i!=-1:
+def backward(i=1):
+    if i>1:
         errorMsg("Sorry Dave, I cannot let you use backward with an argument in this exercise. Use a loop instead.")
     else:
         entity.backward()
