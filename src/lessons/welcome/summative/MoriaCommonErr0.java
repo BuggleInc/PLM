92d992eaaf2c4b782c13c60d4ca5ee606645905a
Matthieu Nicolas <nicolasmatthieu57@gmail.com>
2015-10-09 14:28:30 +0200

    Fix MoriaCommonErr0

--- src/lessons/welcome/summative/MoriaCommonErr0.java
+++ src/lessons/welcome/summative/MoriaCommonErr0.java
@@ -4,23 +4,10 @@ import plm.universe.bugglequest.SimpleBuggle;
 
 public class MoriaCommonErr0 extends SimpleBuggle {
 	@Override
-	public void forward(int i)  { 
-		throw new RuntimeException(getGame().i18n.tr("Sorry Dave, I cannot let you use forward with an argument in this exercise. Use a loop instead."));
-	}
-
-	@Override
-	public void backward(int i) {
-		throw new RuntimeException(getGame().i18n.tr("Sorry Dave, I cannot let you use backward with an argument in this exercise. Use a loop instead."));
-	}
-
-	@Override
-	public void run() { 
+	public void run() {
 		back();
 		while(!isFacingWall()) {
-			while(!isOverBaggle()) {
-				forward();
-			}
-			pickupBaggle();
+			forward();
 		}
 	}
 }
