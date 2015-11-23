7fbed3c09ddf72dfe4f57d304158cfeb0db625cf
Matthieu Nicolas <nicolasmatthieu57@gmail.com>
2015-11-23 11:44:43 +0100

    Add new constructor to GitUtils to remove dependency to Game

--- src/plm/core/model/tracking/GitUtils.java
+++ src/plm/core/model/tracking/GitUtils.java
@@ -42,6 +42,7 @@ import org.eclipse.jgit.transport.PushResult;
 import org.eclipse.jgit.transport.RefSpec;
 import org.eclipse.jgit.transport.RemoteRefUpdate;
 import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
+import org.xnap.commons.i18n.I18n;
 
 import plm.core.model.Game;
 import plm.core.model.LogHandler;
@@ -54,8 +55,15 @@ public class GitUtils {
 
 	private String username = "";
 	private String password = Game.getProperty("plm.git.server.password");
+	private LogHandler logger;
+	private I18n i18n;
 
 	private static boolean currentlyPushing = false;
+
+	public GitUtils(LogHandler logger, I18n i18n) {
+		this.logger = logger;
+		this.i18n = i18n;
+	}
 
 	public GitUtils(String username) {
 		this.username = username;
@@ -63,6 +71,8 @@ public class GitUtils {
 
 	public void setGame(Game g) {
 		this.game = g;
+		this.logger = g.getLogger();
+		this.i18n = g.i18n;
 	}
 
 	public File getRepoDir() {
@@ -82,7 +92,7 @@ public class GitUtils {
 		try {
 			cfg.save();
 		} catch (IOException e) {
-			//game.getLogger().log(getGame().i18n.tr("An error occurred while configuring the repository..."));
+			logger.log(i18n.tr("An error occurred while configuring the repository..."));
 			e.printStackTrace();
 		}
 	}
@@ -91,7 +101,7 @@ public class GitUtils {
 		//if(!getGame().getTrackUser()) {
 		//	return false;
 		//}
-		//game.getLogger().log(getGame().i18n.tr("Retrieving your session from the servers..."));
+		logger.log(i18n.tr("Retrieving your session from the servers..."));
 		try {
 			git.fetch().setCheckFetchedObjects(true).setRefSpecs(new RefSpec("+refs/heads/"+userBranchHash+":refs/remotes/origin/"+userBranchHash)).call();
 		} catch (GitAPIException ex) {
@@ -125,7 +135,7 @@ public class GitUtils {
 		try {
 			git.checkout().setName(userBranchHash).call();
 		} catch (GitAPIException e) {
-			//game.getLogger().log(getGame().i18n.tr("An error occurred while checking out the user's branch: ")+userBranchHash);
+			logger.log(i18n.tr("An error occurred while checking out the user's branch: ")+userBranchHash);
 			// FIXME: display the stacktrace if debug mode is enabled
 			//e.printStackTrace();
 			success = false;
@@ -138,13 +148,13 @@ public class GitUtils {
 			MergeResult res = git.merge().setCommit(true).setFastForward(MergeCommand.FastForwardMode.FF).setStrategy(MergeStrategy.RECURSIVE).include(git.getRepository().getRef("refs/remotes/origin/"+userBranchHash)).call();
 
 			if(res.getMergeStatus() == MergeResult.MergeStatus.FAST_FORWARD) {
-				//game.getLogger().log(getGame().i18n.tr("last session data successfully retrieved"));
+				logger.log(i18n.tr("last session data successfully retrieved"));
 			}
 			else if(res.getMergeStatus() == MergeResult.MergeStatus.MERGED) {
-				//game.getLogger().log(getGame().i18n.tr("last session data successfully merged"));
+				logger.log(i18n.tr("last session data successfully merged"));
 			}
 			else if(res.getMergeStatus() == MergeResult.MergeStatus.CONFLICTING) {
-				//game.getLogger().log(getGame().i18n.tr("Conflicts have been detected while synchronizing with last session data, trying to resolve it..."));
+				logger.log(i18n.tr("Conflicts have been detected while synchronizing with last session data, trying to resolve it..."));
 				Map<String, int[][]> allConflicts = res.getConflicts();
 				for (String path : allConflicts.keySet()) {
 					ObjectId remote = git.getRepository().resolve("origin/"+userBranchHash);
@@ -162,7 +172,8 @@ public class GitUtils {
 					}
 					git.add().addFilepattern(path).call();
 				}
-				//game.getLogger().log("All conflicts have been manually handled ;)");
+				
+				logger.log("All conflicts have been manually handled ;)");
 				// TODO: check if the commit is mandatory
 				git.commit().setMessage("Manual merging")
 				.setAuthor(new PersonIdent("John Doe", "john.doe@plm.net"))
@@ -171,14 +182,14 @@ public class GitUtils {
 			}
 			else if(res.getMergeStatus() == MergeResult.MergeStatus.FAILED) {
 				// TODO: handle this case
-				//game.getLogger().log(getGame().i18n.tr("Canceled the merge operation because of the following failures:"));
+				logger.log(i18n.tr("Canceled the merge operation because of the following failures:"));
 				Map<String, MergeFailureReason> allFailures = res.getFailingPaths();
 				for(String path : allFailures.keySet()) {
-					//game.getLogger().log(path + " : " + allFailures.get(path));
+					logger.log(path + " : " + allFailures.get(path));
 				}
 			}
 		} catch (Exception ex) {
-			//System.err.println(getGame().i18n.tr("Can't merge data retrieved from server with local session data."));
+			System.err.println(i18n.tr("Can't merge data retrieved from server with local session data."));
 			throw ex;
 		}
 	}
@@ -219,7 +230,7 @@ public class GitUtils {
 		} catch (InvalidRemoteException e) {
 			e.printStackTrace();
 		} catch (TransportException e) {
-			//game.getLogger().log(getGame().i18n.tr("Cannot synchronize your session with the servers (network down)."));
+			logger.log(i18n.tr("Cannot synchronize your session with the servers (network down)."));
 			if (game.isDebugEnabled())
 				e.printStackTrace();
 		} catch (GitAPIException e) {
@@ -246,21 +257,21 @@ public class GitUtils {
 
 		// push
 		if(pushChanges(userBranchHash, progress, cp)) {
-			//game.getLogger().log(getGame().i18n.tr("Your session has been successfully saved into the clouds."));
+			logger.log(i18n.tr("Your session has been successfully saved into the clouds."));
 		}
 		else {
 			// An error occurred while pushing
-			//game.getLogger().log(getGame().i18n.tr("Fetching the server's last version..."));
+			logger.log(i18n.tr("Fetching the server's last version..."));
 			try {
 				// Try to synchronize with the remote branch before pushing again
 				if (fetchBranchFromRemoteBranch(userBranchHash)) {
 					mergeRemoteIntoLocalBranch(userBranchHash);
 				}
 				if(!pushChanges(userBranchHash, progress, cp)) {
-					//game.getLogger().log(getGame().i18n.tr("Fetching the data's last version didn't solve the issue, please report this bug."));
+					logger.log(i18n.tr("Fetching the data's last version didn't solve the issue, please report this bug."));
 				}
 			} catch (Exception e) {
-				//game.getLogger().log(getGame().i18n.tr("A bug occurred while synchronizing your data with the server, please report the following error:"));
+				logger.log(i18n.tr("A bug occurred while synchronizing your data with the server, please report the following error:"));
 				e.printStackTrace();
 			}
 		}
