4f89f724f5db330afa72a17333160a2f11d98dd0
Matthieu Nicolas <nicolasmatthieu57@gmail.com>
2016-03-14 13:58:32 +0100

    Remove random generation of Pancake worlds

--- src/lessons/sort/pancake/BasicPancake.java
+++ src/lessons/sort/pancake/BasicPancake.java
@@ -11,12 +11,15 @@ public class BasicPancake extends ExerciseTemplated {
 	public BasicPancake(Game game, Lesson lesson) {
 		super(game, lesson);
 	
-		PancakeWorld plate[] = new PancakeWorld[4];
-		plate[0]= new PancakeWorld(game, "5 pancakes",5,false);
-		plate[1]= new PancakeWorld(game, "10 pancakes",10,false);
-		plate[2]= new PancakeWorld(game, "15 pancakes",15,false);
-		plate[3]= new PancakeWorld(game, "30 pancakes",30,false);
-		for ( int i = 0 ; i<4;i++)
+		PancakeWorld[] plate = new PancakeWorld[] {
+				new PancakeWorld(game, "5 pancakes",new int[] {5,1,3,4,2},false),
+				new PancakeWorld(game, "10 pancakes",new int[] {1,9,2,7,10,5,4,6,8,3},false),
+				new PancakeWorld(game, "15 pancakes",new int[] {14,3,13,10,11,12,2,8,7,6,1,15,5,9,4},false),
+				new PancakeWorld(game, "30 pancakes",new int[] 
+						{13,30,28,11,26,14,24,3,23,21,20,29,18,22,15,16,25,19,4,27,10,5,1,7,6,9,12,2,17,8},
+						false),
+		};
+		for ( int i = 0 ; i<plate.length;i++)
 			new PancakeEntity("Pancake Seller",plate[i]);
 
 		setup(plate);
--- src/lessons/sort/pancake/BubblePancake.java
+++ src/lessons/sort/pancake/BubblePancake.java
@@ -11,12 +11,15 @@ public class BubblePancake extends ExerciseTemplated {
 	public BubblePancake(Game game, Lesson lesson) {
 		super(game, lesson);
 	
-		PancakeWorld plate[] = new PancakeWorld[4];
-		plate[0]= new PancakeWorld(game, "5 pancakes",5,false);
-		plate[1]= new PancakeWorld(game, "10 pancakes",10,false);
-		plate[2]= new PancakeWorld(game, "15 pancakes",15,false);
-		plate[3]= new PancakeWorld(game, "30 pancakes",30,false);
-		for ( int i = 0 ; i<4;i++)
+		PancakeWorld[] plate = new PancakeWorld[] {
+				new PancakeWorld(game, "5 pancakes",new int[] {5,2,3,4,1},false),
+				new PancakeWorld(game, "10 pancakes",new int[] {10,3,8,1,9,5,4,7,6,2},false),
+				new PancakeWorld(game, "15 pancakes",new int[] {8,12,13,14,11,3,2,7,6,15,5,4,9,10,1},false),
+				new PancakeWorld(game, "30 pancakes",new int[] 
+						{5,29,8,9,27,25,4,11,3,28,7,19,18,10,16,12,26,22,15,23,13,30,2,24,14,20,6,1,17,21},
+						false),
+		};
+		for ( int i = 0 ; i<plate.length;i++)
 			new PancakeEntity("Pancake Seller",plate[i]);
 
 		setup(plate);
