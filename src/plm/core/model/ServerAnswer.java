3fa2fe5f0bdf1375c439c60a5c97a8cbb3cd8571
Martin Quinson <martin.quinson@loria.fr>
2017-09-02 18:49:00 +0200

    remove unused code

--- src/plm/core/model/ServerAnswer.java
+++
@@ -1,8 +0,0 @@
-package plm.core.model;
-
-/**
- * Enum that contains all answers that the server can send (errors or success messages)
- */
-public enum ServerAnswer {
-	ALL_IS_FINE, WRONG_PASSWORD, WRONG_TEACHER_PASSWORD, COURSE_NAME_ALREADY_USED, DATA_NOT_IN_DATABASE
-}
