e826ec0dcf5a504aa3e4f4916f34a90f75f1987c
Martin Quinson <martin.quinson@loria.fr>
2017-10-10 21:03:03 +0200

    also restore the string1 lesson back from the death

---
+++ resources/exercises/bat/string1/StringBits.java
@@ -0,0 +1,53 @@
+package lessons.bat.string1;
+import plm.core.model.lesson.Lesson;
+import plm.universe.bat.BatExercise;
+import plm.universe.bat.BatTest;
+import plm.universe.bat.BatWorld;
+
+public class StringBits extends BatExercise {
+	public StringBits(Lesson lesson) {
+		super(lesson);
+
+		BatWorld myWorld = new BatWorld("stringBits");
+		myWorld.addTest(VISIBLE, "Hello") ;
+		myWorld.addTest(VISIBLE, "Hi") ;
+		myWorld.addTest(VISIBLE, "HiHiHi") ;
+		myWorld.addTest(INVISIBLE, "") ;
+		myWorld.addTest(INVISIBLE, "Greetings") ;
+
+		templatePython("stringBits", new String[] {"String"},
+				"def stringBits(str):\n",
+				"  res = ''\n" +
+				"  for i in range(0,len(str),2):\n" +
+				"    res += str[i:i+1]\n" +
+				"  return res\n");
+		templateScala("stringBits", new String[] {"String"}, 
+				"def stringBits(str:String):String = {\n",
+				"  var res:String = \"\"\n" +
+				"  for (i <- 0 to str.length-1 by 2)\n" +
+				"    res += str.substring(i,i+1)\n" +
+				"  return res\n"+
+				"}");
+		setup(myWorld);
+	}
+
+	public void run(BatTest t) {
+		/* BEGIN SKEL */
+		t.setResult( stringBits((String)t.getParameter(0)) );
+		/* END SKEL */
+	}
+
+	/* BEGIN TEMPLATE */
+	String stringBits(String str) {
+		/* BEGIN SOLUTION */
+		String result = "";
+		// Note: the loop increments i by 2
+		for (int i=0; i<str.length(); i+=2) {
+			result = result + str.substring(i, i+1);
+			// Alternately could use str.charAt(i)
+		}
+		return result;
+		/* END SOLUTION */
+	}
+	/* END TEMPLATE */
+}
---
+++ resources/exercises/bat/string1/StringMatch.java
@@ -0,0 +1,69 @@
+package lessons.bat.string1;
+import plm.core.model.lesson.Lesson;
+import plm.universe.bat.BatExercise;
+import plm.universe.bat.BatTest;
+import plm.universe.bat.BatWorld;
+
+public class StringMatch extends BatExercise {
+	public StringMatch(Lesson lesson) {
+		super(lesson);
+
+		BatWorld myWorld = new BatWorld("stringMatch");
+		myWorld.addTest(VISIBLE, "xxcaazz", "xxbaaz") ;
+		myWorld.addTest(VISIBLE, "abc", "abc") ;
+		myWorld.addTest(VISIBLE, "abc", "axc") ;
+		myWorld.addTest(INVISIBLE, "hello", "he") ;
+		myWorld.addTest(INVISIBLE, "he", "hello") ;
+		myWorld.addTest(INVISIBLE, "h", "hello") ;
+		myWorld.addTest(INVISIBLE, "", "hello") ;
+		myWorld.addTest(INVISIBLE, "aabbccdd", "abbbxxd") ;
+		myWorld.addTest(INVISIBLE, "aaxxaaxx", "iaxxai") ;
+		myWorld.addTest(INVISIBLE, "iaxxai", "aaxxaaxx") ;
+
+		templatePython("stringMatch", new String[]{"String","String"},
+				"def stringMatch(a, b):\n",
+				"  l = min( len(a), len(b) )\n" +
+				"  count = 0\n" +
+				"  for i in range(l-1):\n" +
+				"    if a[i:i+2] == b[i:i+2]:\n" +
+				"      count += 1\n" +
+				"  return count\n");
+		templateScala("stringMatch", new String[]{"String","String"},
+				"def stringMatch(a:String, b:String):Int = {\n",
+				"  val l = Math.min( a.length, b.length )\n" +
+				"  var count = 0\n" +
+				"  for (i <- 0 to l-2)\n" +
+				"    if (a.substring(i,i+2) == b.substring(i,i+2))\n" +
+				"      count += 1\n" +
+				"  return count\n"+
+				"}");
+		setup(myWorld);
+	}
+
+	public void run(BatTest t) {
+		/* BEGIN SKEL */
+		t.setResult( stringMatch((String)t.getParameter(0), (String)t.getParameter(1)) );
+		/* END SKEL */
+	}
+
+	/* BEGIN TEMPLATE */
+	int stringMatch(String a, String b) {
+		/* BEGIN SOLUTION */
+		// Figure which string is shorter.
+		int len = Math.min(a.length(), b.length());
+		int count = 0;
+
+		// Look at both substrings starting at i
+		for (int i=0; i<len-1; i++) {
+			String aSub = a.substring(i, i+2);
+			String bSub = b.substring(i, i+2);
+			if (aSub.equals(bSub)) {  // Use .equals() with strings
+				count++;
+			}
+		}
+
+		return count;
+		/* END SOLUTION */
+	}
+	/* END TEMPLATE */
+}
