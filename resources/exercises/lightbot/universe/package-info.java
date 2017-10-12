bc7fa3defa17c81dc0bd935bcda44adabee059cd
Martin Quinson <martin.quinson@loria.fr>
2017-10-13 00:41:47 +0200

    lightbot will not work anytime soon

--- resources/exercises/lightbot/universe/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Little programming game universe where you must program a robot to 
- * light up some lights. You do so graphically and not in java. 
- */
-package lessons.lightbot.universe;
-
