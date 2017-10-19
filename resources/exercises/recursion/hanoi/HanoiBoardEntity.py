1fc2e081c312c8d5e27a46674d4a611eeb44d7be
Martin Quinson <martin.quinson@loria.fr>
2017-10-19 16:08:17 +0200

    Cosmetics in hanoi python entities, actually fixing bugs on the way
    The most important issues where errors such as:
    Syntax error: mismatched input '' expecting DEDENT

--- resources/exercises/recursion/hanoi/HanoiBoardEntity.py
+++ resources/exercises/recursion/hanoi/HanoiBoardEntity.py
@@ -1,12 +1,13 @@
 # BEGIN TEMPLATE
-def hanoi(height, src,other,dst):
-	
-	# Your code here
-	# BEGIN SOLUTION
-	if height != 0:
-		hanoi(height-1, src,dst,other);
-		move(src,dst);
-		hanoi(height-1, other,src,dst);
-	# END SOLUTION
+def hanoi(height, src, other, dst):
+    # Your code here
+    # BEGIN SOLUTION
+    if height != 0:
+        hanoi(height - 1, src, dst, other)
+        move(src, dst)
+        hanoi(height - 1, other, src, dst)
+    # END SOLUTION
+
+
 # END TEMPLATE
-hanoi(getSlotSize(getParam(0)), getParam(0),getParam(1),getParam(2))
+hanoi(getSlotSize(getParam(0)), getParam(0), getParam(1), getParam(2))
