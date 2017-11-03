b4dc0f0f600d44591671ef752b6c7375521b581e
Martin Quinson <martin.quinson@loria.fr>
2017-11-03 22:35:55 +0100

    typo
    Adapts df73bcb8eac12043a893e41f1ea0c2348d543f2a from javaUI

--- src/plm/core/lang/LangScala.java
+++ src/plm/core/lang/LangScala.java
@@ -116,7 +116,7 @@ class ScalaCompiler {
 
 	public void compile(String name,String content,int offset) throws PLMCompilerException {
 		if (isDebugEnabled)
-			System.out.println("Compiline souce "+name+" to scala (offset:"+offset+"):\n"+content);
+			System.out.println("Compiling souce "+name+" to scala (offset:"+offset+"):\n"+content);
 
 		Run compiler = global.new Run();
 		List<SourceFile> sources = new LinkedList<SourceFile>();
