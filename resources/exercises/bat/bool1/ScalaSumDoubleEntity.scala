3a943c19b0352ad6bf66644a83b0e4172767e243
Martin Quinson <martin.quinson@loria.fr>
2018-01-06 23:09:15 +0100

    fix the scala solution of that exo

--- resources/exercises/bat/bool1/ScalaSumDoubleEntity.scala
+++ resources/exercises/bat/bool1/ScalaSumDoubleEntity.scala
@@ -3,9 +3,6 @@ package bat.bool1
 import plm.universe.bat.BatTest
 import plm.universe.bat.BatEntity
 
-/**
- * @author matthieu
- */
 class ScalaSumDoubleEntity extends BatEntity {
   override def run(t: BatTest) {
     t.setResult( sumDouble(t.getParameter(0).asInstanceOf[Integer],t.getParameter(1).asInstanceOf[Integer]))
@@ -14,10 +11,10 @@ class ScalaSumDoubleEntity extends BatEntity {
   /* BEGIN TEMPLATE */
   def sumDouble(a: Integer, b: Integer): Integer = {
     /* BEGIN SOLUTION */
-    if(a==b) {
+    if(a==b) 
       (a+b)*2
-    }
-    a+b
+    else 
+      a+b
     /* END SOLUTION */
   }
   /* END TEMPLATE */
