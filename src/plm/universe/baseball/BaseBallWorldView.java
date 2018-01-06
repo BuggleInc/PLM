aeda908805baac3bc192b08382c94cfd7f4a2eec
Martin Quinson <martin.quinson@loria.fr>
2018-01-06 23:10:17 +0100

    small cleanups and reduce the amount of new() to speedup things

--- src/plm/universe/baseball/BaseBallWorldView.java
+++ src/plm/universe/baseball/BaseBallWorldView.java
@@ -4,9 +4,7 @@ import org.jfree.graphics2d.svg.SVGGraphics2D;
 
 import plm.universe.World;
 import plm.universe.WorldView;
-import plm.universe.turtles.ResourcesCache;
 
-import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.QuadCurve2D;
@@ -18,19 +16,18 @@ import java.util.*;
 
 public class BaseBallWorldView extends WorldView {
 
-    private static final long serialVersionUID = 1L;
-    private static Vector<Integer[]> playersCoordinate;
     private static double radius;
     private static double displayRatio;
     private static double virtualSize = 300.; // we display on a world that is 300x300 and dynamically resized (to ease our computations)
 
     private static boolean useCircularView = true; // historical view if false
-    private JPopupMenu popup = new JPopupMenu();
 
     public BaseBallWorldView(World w) {
         super(w);
     }
 
+    static int[] arrowXPoints = new int[ 3 ] ;
+    static int[] arrowYPoints = new int[ 3 ] ;
     /**
      * Draws an arrow on the given Graphics2D context
      * ( adapted from http://www.bytemycode.com/snippets/snippet/82/ )
@@ -43,8 +40,6 @@ public class BaseBallWorldView extends WorldView {
     private static void drawArrow(Graphics2D g, int xTail, int yTail, int xHead, int yHead) {
         float arrowWidth = 12.0f ;
         float theta = 0.6f ;
-        int[] xPoints = new int[ 3 ] ;
-        int[] yPoints = new int[ 3 ] ;
         float[] vecLine = new float[] {(float)xHead - xTail, (float)yHead - yTail};
         float[] vecLeft = new float[] {-vecLine[ 1 ],vecLine[ 0 ]} ; // arrow base vector - normal to the line
         float fLength;
@@ -52,8 +47,8 @@ public class BaseBallWorldView extends WorldView {
         float ta;
         float baseX, baseY ;
 
-        xPoints[ 0 ] = xHead ;
-        yPoints[ 0 ] = yHead ;
+        arrowXPoints[ 0 ] = xHead ;
+        arrowYPoints[ 0 ] = yHead ;
 
         // setup length parameters
         fLength = (float)Math.sqrt( vecLine[0] * vecLine[0] + vecLine[1] * vecLine[1] ) ;
@@ -61,19 +56,20 @@ public class BaseBallWorldView extends WorldView {
         ta = arrowWidth / ( 2.0f * ( (float)Math.tan( theta ) / 2.0f ) * fLength ) ;
 
         // find the base of the arrow
-        baseX = ( (float)xPoints[ 0 ] - ta * vecLine[0]);
-        baseY = ( (float)yPoints[ 0 ] - ta * vecLine[1]);
+        baseX = ( (float)arrowXPoints[ 0 ] - ta * vecLine[0]);
+        baseY = ( (float)arrowYPoints[ 0 ] - ta * vecLine[1]);
 
         // build the points on the sides of the arrow
-        xPoints[ 1 ] = (int)( baseX + th * vecLeft[0] );
-        yPoints[ 1 ] = (int)( baseY + th * vecLeft[1] );
-        xPoints[ 2 ] = (int)( baseX - th * vecLeft[0] );
-        yPoints[ 2 ] = (int)( baseY - th * vecLeft[1] );
+        arrowXPoints[ 1 ] = (int)( baseX + th * vecLeft[0] );
+        arrowYPoints[ 1 ] = (int)( baseY + th * vecLeft[1] );
+        arrowXPoints[ 2 ] = (int)( baseX - th * vecLeft[0] );
+        arrowYPoints[ 2 ] = (int)( baseY - th * vecLeft[1] );
 
         g.drawLine( xTail, yTail, (int)baseX, (int)baseY ) ;
-        g.fillPolygon( xPoints, yPoints, 3 ) ;
+        g.fillPolygon( arrowXPoints, arrowYPoints, 3 ) ;
     }
 
+    static int[][] cornerPoints = new int[4][2];
     /**
      * Compute the corners of the base located at polar coordinates ( radius, theta ) and return an array
      * which contains the coordinates of the corners of the base
@@ -85,29 +81,30 @@ public class BaseBallWorldView extends WorldView {
      * @return an array containing the coordinates of the four corners of the base
      */
     private static int[][] computeCorners(int L, int radius, double theta, int xCenter, int yCenter) {
-        int[][] points = new int[4][2];
         // Prevent some redundancies during the computing
         double commonPart = L*Math.sin(theta);
         double rightPart = (radius+L/2)*Math.cos(theta);
         double leftPart = (radius-L/2)*Math.cos(theta) ;
 
-        points[0][0] = (int) (xCenter - commonPart + rightPart); // x coordinate of the upper-right point when theta = 0
-        points[1][0] = (int) (xCenter + commonPart + rightPart); // x coordinate of the lower-right point when theta = 0
-        points[2][0] = (int) (xCenter + commonPart + leftPart); // x coordinate of the lower-left point when theta = 0
-        points[3][0] = (int) (xCenter - commonPart + leftPart); // x coordinate of the upper-left point when theta = 0
+        cornerPoints[0][0] = (int) (xCenter - commonPart + rightPart); // x coordinate of the upper-right point when theta = 0
+        cornerPoints[1][0] = (int) (xCenter + commonPart + rightPart); // x coordinate of the lower-right point when theta = 0
+        cornerPoints[2][0] = (int) (xCenter + commonPart + leftPart); // x coordinate of the lower-left point when theta = 0
+        cornerPoints[3][0] = (int) (xCenter - commonPart + leftPart); // x coordinate of the upper-left point when theta = 0
 
         commonPart = L*Math.cos(theta);
         rightPart = -(radius+L/2)*Math.sin(theta);
         leftPart = -(radius-L/2)*Math.sin(theta) ;
 
-        points[0][1] = (int) (yCenter - commonPart + rightPart); // y coordinate of the upper-right point when theta = 0
-        points[1][1] = (int) (yCenter + commonPart + rightPart); // y coordinate of the lower-right point when theta = 0
-        points[2][1] = (int) (yCenter + commonPart + leftPart); // y coordinate of the lower-left point when theta = 0
-        points[3][1] = (int) (yCenter - commonPart + leftPart); // y coordinate of the upper-left point when theta = 0
+        cornerPoints[0][1] = (int) (yCenter - commonPart + rightPart); // y coordinate of the upper-right point when theta = 0
+        cornerPoints[1][1] = (int) (yCenter + commonPart + rightPart); // y coordinate of the lower-right point when theta = 0
+        cornerPoints[2][1] = (int) (yCenter + commonPart + leftPart); // y coordinate of the lower-left point when theta = 0
+        cornerPoints[3][1] = (int) (yCenter - commonPart + leftPart); // y coordinate of the upper-left point when theta = 0
 
-        return points;
+        return cornerPoints;
     }
 
+    static int[] baseXPoints = new int[4];
+    static int[] baseYPoints = new int[4];
     /**
      * Draw the base and the players
      * @param g The Graphics2D context to draw on
@@ -122,22 +119,16 @@ public class BaseBallWorldView extends WorldView {
     private static void drawBase(Graphics2D g,BaseballWorld world, int L, int dist, double theta, int xCenter, int yCenter, int base, int amountOfPlayers) {
         int[][] points = computeCorners( L, dist, theta, xCenter, yCenter );
 
-        // draw the base
-        // will contains the x coordinates of the points
-        int[] xPoints = new int[4];
-        // will contains the y coordinates of the points
-        int[] yPoints = new int[4];
-        // fill the arrays mentioned earlier
         for (int i = 0 ; i < 4 ; i++) {
-            xPoints[i] = points[i][0];
-            yPoints[i] = points[i][1];
+            baseXPoints[i] = points[i][0];
+            baseYPoints[i] = points[i][1];
         }
         // draw the shape of the base
         g.setColor(Color.BLACK);
-        g.drawPolygon(xPoints, yPoints, 4);
+        g.drawPolygon(baseXPoints, baseYPoints, 4);
         // fill this shape
         g.setColor(obtainColor(base));
-        g.fillPolygon(xPoints, yPoints, 4);
+        g.fillPolygon(baseXPoints, baseYPoints, 4);
 
         // the radius of the disk representing the player. the graphically depicted is a bit smaller than the clickable
         radius = (L/amountOfPlayers-1); // clickable
@@ -180,10 +171,6 @@ public class BaseBallWorldView extends WorldView {
 
             colorPlayer = obtainColor(player);
 
-            playersCoordinate.add(new Integer[] {
-                    centerPlayer[0], centerPlayer[1],
-                    base,pos
-            });
             //System.out.println("center: "+centerPlayer[0]+","+centerPlayer[1]+" radius: "+this.radius+" player "+base+","+pos+" color:"+colorPlayer);
             drawPlayer(g, centerPlayer, radiusbis, colorPlayer, theta, base==player || player == -1);
         }
@@ -231,30 +218,30 @@ public class BaseBallWorldView extends WorldView {
         }
     }
 
+    private static Color[] colorList = {
+    		Color.WHITE, Color.BLUE, new Color(255,0,255), //Color.YELLOW,
+    		new Color(255,204,0), /* gold */
+    		new Color(158,253,56), /* French lime */
+    		new Color(255,56,0), /* Coquelicot */
+    		new Color(204,204,255), /* Lavender blue */
+    		new Color(0,103,165), /* Blue Persian */
+    		new Color(201,0,22), /* Harvard crimson */
+    		new Color(111,78,55), /* Coffee */
+    		new Color(251,206,177), /* Apricot */
+    		new Color(109,7,26), /* Bordeaux */
+    		new Color(155,150,10),
+    		new Color(75,0,130),
+    		new Color(150,85,120),
+    		Color.GREEN
+    };
     /** Returns the color corresponding to colorIndex */
     private static Color obtainColor(int colorIndex) {
-        Color[] colors = {
-                Color.WHITE, Color.BLUE, new Color(255,0,255), //Color.YELLOW,
-                new Color(255,204,0), /* gold */
-                new Color(158,253,56), /* French lime */
-                new Color(255,56,0), /* Coquelicot */
-                new Color(204,204,255), /* Lavender blue */
-                new Color(0,103,165), /* Blue Persian */
-                new Color(201,0,22), /* Harvard crimson */
-                new Color(111,78,55), /* Coffee */
-                new Color(251,206,177), /* Apricot */
-                new Color(109,7,26), /* Bordeaux */
-                new Color(155,150,10),
-                new Color(75,0,130),
-                new Color(150,85,120),
-                Color.GREEN
-        };
         Color colorSent ;
-        if ( colorIndex < -1 || colorIndex > colors.length-1 ) {
+        if ( colorIndex < -1 || colorIndex > colorList.length-1 ) {
             System.out.println("Unexpected colorIndex : "+colorIndex+"\nYou should add some colors in BaseballWorldView.obtainColor");
             colorSent = Color.MAGENTA;
         } else {
-            colorSent = colors[colorIndex+1];
+            colorSent = colorList[colorIndex+1];
         }
         return colorSent;
     }
@@ -289,7 +276,6 @@ public class BaseBallWorldView extends WorldView {
         radius+=amountOfBases-5; // adapting the position of the base to the total amount of bases
 
         // Draw the bases and the players on each base
-        playersCoordinate = new Vector<Integer[]>();
         for ( int i=0 ; i < world.getBasesAmount();i++)
             drawBase(g, world, L, radius, theta*i, (int) virtualSize/2, (int) virtualSize/2 , i, amountOfPlayers);
 
