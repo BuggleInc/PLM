3bff8b9b57017701361b1631624b1c18b667e121
Martin Quinson <martin.quinson@ens-rennes.fr>
2020-09-20 15:19:44 +0200

    Improve SlugTracking

--- src/lessons/welcome/methods/slug/SlugTracking.java
+++ src/lessons/welcome/methods/slug/SlugTracking.java
@@ -18,20 +18,26 @@ public class SlugTracking extends ExerciseTemplated {
 
 		BuggleWorld myWorld = new BuggleWorld("Forrest", 8, 7);
 		for (int i = 5; i >= 2; i--)
-			myWorld.setColor(6, i,Color.green);
-		myWorld.setColor(5, 2,Color.green);
+			myWorld.setColor(5, i,Color.green);
+		myWorld.setColor(4, 2,Color.green);
 		for (int i = 2; i <= 4; i++)
-			myWorld.setColor(4, i,Color.green);
-		myWorld.setColor(3, 4,Color.green);
+			myWorld.setColor(3, i,Color.green);
+		myWorld.setColor(2, 4,Color.green);
 		for (int i = 4; i >= 1; i--)
-			myWorld.setColor(2, i,Color.green);
-		myWorld.setColor(1, 1,Color.green);
+			myWorld.setColor(1, i,Color.green);
 		myWorld.setColor(0, 1,Color.green);
+		myWorld.putTopWall(5, 5);
+		myWorld.setColor(6, 4,Color.green);
+		myWorld.setColor(6, 5,Color.green);
+		myWorld.putLeftWall(7, 4);
+		myWorld.putLeftWall(7, 5);
 		try {
 			myWorld.addBaggle(0, 1);
 		} catch (AlreadyHaveBaggleException e) {
 			e.printStackTrace();
 		}
+		SimpleBuggle hunter = new SimpleBuggle(myWorld, "Hunter", 5, 6, Direction.NORTH, Color.black, Color.lightGray);
+		hunter.brushDown();
 		myWorlds[0] = myWorld;
 
 		myWorld = new BuggleWorld("Desert", 8, 7);
@@ -58,9 +64,6 @@ public class SlugTracking extends ExerciseTemplated {
 			e.printStackTrace();
 		}
 		myWorlds[1] = myWorld;
-
-		SimpleBuggle hunter = new SimpleBuggle(myWorlds[0], "Hunter", 6, 6, Direction.NORTH, Color.black, Color.lightGray);
-		hunter.brushDown();
 
 		hunter = new SimpleBuggle(myWorlds[1], "Hunter", 6, 6, Direction.NORTH, Color.black, Color.lightGray);
 		hunter.brushDown();
