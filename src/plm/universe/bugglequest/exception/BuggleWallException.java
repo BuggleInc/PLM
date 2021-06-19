63ae35fee2aa8f8c6a741f26f05c60282404e003
Martin Quinson <martin.quinson@ens-rennes.fr>
2021-06-20 00:46:18 +0200

    Also use stderr when hitting a wall

--- src/plm/universe/bugglequest/exception/BuggleWallException.java
+++ src/plm/universe/bugglequest/exception/BuggleWallException.java
@@ -8,6 +8,7 @@ public class BuggleWallException extends PLMException {
 
 	public BuggleWallException() {
 		super(Game.i18n.tr("Buggles cannot traverse walls"));
+		System.err.println("Buggles cannot traverse walls");
 	}
 
 	private static final long serialVersionUID = -7246709356730960089L;
