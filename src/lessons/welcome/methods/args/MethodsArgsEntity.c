f97846d49aee7d902da423964c42f4824c5d41ed
Martin Quinson <martin.quinson@ens-rennes.fr>
2026-06-27 11:24:29 +0200

    Install Dave-like protections (anti cheating) to MethodsArgsEntity.c

--- src/lessons/welcome/methods/args/MethodsArgsEntity.c
+++ src/lessons/welcome/methods/args/MethodsArgsEntity.c
@@ -1,25 +1,43 @@
-//RemoteBuggle
+#include "../../../../../lib/resources/langages/c/include/RemoteBuggle.h"
 
 /* BEGIN TEMPLATE */
 /* BEGIN SOLUTION */
-void move(int nbPas, int f) {
+void move(int nbStep, int f)
+{
 
-	int i;
-	if (f) {
-		for (i=0; i<nbPas; i++)
-			forward(1);
-	} else {
-		for (i=0; i<nbPas; i++)
-			backward(1);
-	}
+  int i;
+  if (f) {
+    for (i = 0; i < nbStep; i++)
+      stepForward();
+  } else {
+    for (i = 0; i < nbStep; i++)
+      stepBackward();
+  }
 }
 /* END SOLUTION */
 /* END TEMPLATE */
 
+static void go_forward(int steps)
+{
+  if (steps > 1) {
+    printf("Sorry Dave, I cannot let you use forward with an argument in this exercise. Use a loop instead.");
+    exit(1);
+  }
+}
+static void go_backward(int steps)
+{
+  if (steps > 1) {
+    printf("Sorry Dave, I cannot let you use backward with an argument in this exercise. Use a loop instead.");
+    exit(1);
+  }
+}
+
 void run(){
-	if(getDirection() == NORTH){
-		move(getY(),1);
-	}else{
-		move(getY(),0);
-	}
+  set_pre_forward(go_forward);
+  set_pre_backward(go_backward);
+  if (getDirection() == NORTH) {
+    move(getY(), 1);
+  } else {
+    move(getY(), 0);
+  }
 }
