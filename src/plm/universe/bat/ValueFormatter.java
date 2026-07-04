60bdb1c439ac2ef65f5210351a088ca40bb96054
Martin Quinson <martin.quinson@ens-rennes.fr>
2026-07-04 02:44:47 +0200

    BatExo: move some functions to a specific static class to reduce the noise, and test it

---
+++ src/plm/universe/bat/ValueFormatter.java
@@ -0,0 +1,155 @@
+package plm.universe.bat;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Vector;
+import org.python.core.PyInstance;
+import plm.core.lang.ProgrammingLanguage;
+
+class ValueFormatter {
+  // Collapses List, RecList, Integer[], Vector into one canonical internal type
+  static Object normalize(Object value)
+  {
+    if (value == null)
+      return null;
+    if (value instanceof List) {
+      List<?> l = (List<?>)value;
+      int[] res = new int[l.size()];
+      for (int i = 0; i < l.size(); i++)
+        res[i] = (Integer)l.get(i);
+
+      return res;
+    }
+    if (value instanceof lessons.recursion.cons.universe.RecList) {
+      value = ((lessons.recursion.cons.universe.RecList)value).toArray();
+    }
+    if (value.getClass().isArray() && value.getClass().getComponentType().equals(Integer.class)) {
+      Integer[] orig = (Integer[])value;
+      if (orig.length == 0 || (orig.length == 1 && orig[0] == null))
+        return new int[] {};
+      int[] res = new int[orig.length];
+      for (int i = 0; i < res.length; i++)
+        res[i] = orig[i];
+      return res;
+    }
+    return value;
+  }
+
+  static boolean equals(Object o1, Object o2)
+  {
+    if (o1 == null && o2 == null)
+      return true;
+    if (o1 == null || o2 == null)
+      return false;
+    o1 = ValueFormatter.normalize(o1);
+    o2 = ValueFormatter.normalize(o2);
+    if (o1.getClass().isArray() && o2.getClass().isArray()) {
+      if (!o1.getClass().getComponentType().equals(o2.getClass().getComponentType()))
+        return false; // not same type in both arrays
+
+      if (o1.getClass().getComponentType().equals(Integer.TYPE)) {
+        return Arrays.equals((int[])o1, (int[])o2);
+      } else if (o1.getClass().getComponentType().equals(String.class)) {
+        return Arrays.equals((String[])o1, (String[])o2);
+      } else
+        throw new UnsupportedOperationException("ValueFormatter.equals: unsupported array component type: " +
+                                                o1.getClass().getComponentType().getName());
+    }
+    if (o1.getClass().isArray() || o2.getClass().isArray())
+      return false; // The other cannot be an array because of previous test
+    return o1.equals(o2);
+  }
+
+  /* --- Formatting logic --- */
+  static private void openArray(StringBuilder sb, ProgrammingLanguage pl)
+  {
+    if (pl.isJava()) {
+      sb.append("{");
+    } else if (pl.isScala()) {
+      sb.append("Array(");
+    } else if (pl.isPython()) {
+      sb.append("[");
+    } else {
+      throw new RuntimeException("Please port me to " + pl.getLang());
+    }
+  }
+  static private void closeArray(StringBuilder sb, ProgrammingLanguage pl)
+  {
+    if (pl.isJava())
+      sb.append("}");
+    else if (pl.isScala())
+      sb.append(")");
+    else if (pl.isPython())
+      sb.append("]");
+    else
+      throw new RuntimeException("Please port me to " + pl.getLang());
+  }
+
+  static String format(Object o, ProgrammingLanguage pl)
+  {
+    StringBuilder sb = new StringBuilder();
+    if (o == null) {
+      if (pl.isScala())
+        return "Nil";
+      else if (pl.isPython())
+        return "None";
+      else if (pl.isJava())
+        return "null";
+      else
+        throw new RuntimeException("Please port me to " + pl.getLang());
+    }
+
+    if (o instanceof String[]) {
+      openArray(sb, pl);
+
+      String[] a = (String[])o;
+      for (String i : a)
+        sb.append(i + ",");
+      if (a.length > 0) // Don't kill the last comma if there is none
+        sb.deleteCharAt(sb.length() - 1);
+
+      closeArray(sb, pl);
+
+    } else if (o.getClass().equals(Vector.class) || o.getClass().isArray()) {
+      if (o.getClass().equals(Vector.class))
+        o = ValueFormatter.normalize(o);
+
+      openArray(sb, pl);
+      if (o.getClass().getComponentType().equals(Integer.TYPE)) {
+        int[] a = (int[])o;
+        for (int i : a)
+          sb.append(i + ",");
+
+        if (a.length > 0) // Don't kill the last comma if there is none
+          sb.deleteCharAt(sb.length() - 1);
+      } else if (o.getClass().getComponentType().equals(Integer.class)) {
+        Integer[] a = (Integer[])o;
+        for (Integer i : a)
+          sb.append(i + ",");
+
+        if (a.length > 0) // Don't kill the last comma if there is none
+          sb.deleteCharAt(sb.length() - 1);
+
+      } else {
+        throw new RuntimeException("Unhandled internal type (only Array<int> and Array<Integer> are handled so far)");
+      }
+      closeArray(sb, pl);
+    } else if (o instanceof Boolean) {
+      Boolean b = (Boolean)o;
+      if (pl.isJava() || pl.isScala()) {
+        sb.append(b ? "true" : "false");
+      } else if (pl.isPython()) {
+        sb.append(b ? "True" : "False");
+      } else {
+        throw new RuntimeException("Please port me to " + pl.getLang());
+      }
+    } else if (o instanceof String) {
+      sb.append("\"" + o + "\"");
+    } else if (o instanceof PyInstance) {
+      sb.append(((PyInstance)o).__str__());
+    } else {
+      sb.append(o.toString());
+    }
+    return sb.toString();
+  }
+}
---
+++ src/plm/universe/bat/ValueFormatterTest.java
@@ -0,0 +1,187 @@
+package plm.universe.bat;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Vector;
+import lessons.recursion.cons.universe.RecList;
+import org.junit.jupiter.api.Test;
+import plm.core.lang.ProgrammingLanguage;
+import plm.core.model.Game;
+import plm.universe.bat.ValueFormatter;
+
+/** Tests for ValueFormatter */
+class ValueFormatterTest {
+
+  // ---------------------------------------------------------------
+  // equals() - scalars
+  // ---------------------------------------------------------------
+
+  @Test void equals_bothNull_isTrue() { assertTrue(ValueFormatter.equals(null, null)); }
+
+  @Test void equals_oneNull_isFalse()
+  {
+    assertFalse(ValueFormatter.equals(null, 5));
+    assertFalse(ValueFormatter.equals(5, null));
+  }
+
+  @Test void equals_equalScalars_isTrue()
+  {
+    assertTrue(ValueFormatter.equals(3, 3));
+    assertTrue(ValueFormatter.equals("a", "a"));
+  }
+
+  @Test void equals_differentScalars_isFalse()
+  {
+    assertFalse(ValueFormatter.equals(3, 4));
+    assertFalse(ValueFormatter.equals("a", "b"));
+  }
+
+  // ---------------------------------------------------------------
+  // equals() - integer arrays, across the accepted input shapes
+  // ---------------------------------------------------------------
+
+  @Test void equals_identicalIntArrays_isTrue()
+  {
+    assertTrue(ValueFormatter.equals(new int[] {1, 2, 3}, new int[] {1, 2, 3}));
+  }
+
+  @Test void equals_differentValues_isFalse()
+  {
+    assertFalse(ValueFormatter.equals(new int[] {1, 2, 3}, new int[] {1, 2, 4}));
+  }
+
+  @Test void equals_differentLength_isFalse()
+  {
+    assertFalse(ValueFormatter.equals(new int[] {1, 2}, new int[] {1, 2, 3}));
+  }
+
+  @Test void equals_listOfIntegerVsIntArray_isTrue()
+  {
+    List<Integer> list = Arrays.asList(1, 2, 3);
+    assertTrue(ValueFormatter.equals(list, new int[] {1, 2, 3}));
+  }
+
+  @Test void equals_integerArrayVsIntArray_isTrue()
+  {
+    assertTrue(ValueFormatter.equals(new Integer[] {1, 2, 3}, new int[] {1, 2, 3}));
+  }
+
+  @Test void equals_recListVsIntArray_isTrue()
+  {
+    RecList rl = RecList.fromArray(new int[] {1, 2, 3});
+    assertTrue(ValueFormatter.equals(rl, new int[] {1, 2, 3}));
+  }
+
+  @Test void equals_identicalStringArrays_isTrue()
+  {
+    assertTrue(ValueFormatter.equals(new String[] {"x", "y"}, new String[] {"x", "y"}));
+  }
+
+  // ---------------------------------------------------------------
+  // format() - null per language
+  // ---------------------------------------------------------------
+
+  @Test void format_null_java() { assertEquals("null", ValueFormatter.format(null, javaLang())); }
+
+  @Test void format_null_scala() { assertEquals("Nil", ValueFormatter.format(null, scalaLang())); }
+
+  @Test void format_null_python() { assertEquals("None", ValueFormatter.format(null, pythonLang())); }
+
+  // ---------------------------------------------------------------
+  // format() - int[] per language
+  // ---------------------------------------------------------------
+
+  @Test void format_intArray_java() { assertEquals("{1,2,3}", ValueFormatter.format(new int[] {1, 2, 3}, javaLang())); }
+
+  @Test void format_intArray_scala()
+  {
+    assertEquals("Array(1,2,3)", ValueFormatter.format(new int[] {1, 2, 3}, scalaLang()));
+  }
+
+  @Test void format_intArray_python()
+  {
+    assertEquals("[1,2,3]", ValueFormatter.format(new int[] {1, 2, 3}, pythonLang()));
+  }
+
+  @Test void format_emptyIntArray_noTrailingComma()
+  {
+    assertEquals("{}", ValueFormatter.format(new int[] {}, javaLang()));
+  }
+
+  // ---------------------------------------------------------------
+  // format() - String[]
+  // ---------------------------------------------------------------
+
+  @Test void format_stringArray_java()
+  {
+    assertEquals("{a,b}", ValueFormatter.format(new String[] {"a", "b"}, javaLang()));
+  }
+
+  @Test void format_stringArray_python()
+  {
+    assertEquals("[a,b]", ValueFormatter.format(new String[] {"a", "b"}, pythonLang()));
+  }
+
+  // ---------------------------------------------------------------
+  // format() - Boolean
+  // ---------------------------------------------------------------
+
+  @Test void format_boolean_javaAndScala_useLowercase()
+  {
+    assertEquals("true", ValueFormatter.format(Boolean.TRUE, javaLang()));
+    assertEquals("false", ValueFormatter.format(Boolean.FALSE, scalaLang()));
+  }
+
+  @Test void format_boolean_python_usesCapitalized()
+  {
+    assertEquals("True", ValueFormatter.format(Boolean.TRUE, pythonLang()));
+    assertEquals("False", ValueFormatter.format(Boolean.FALSE, pythonLang()));
+  }
+
+  // ---------------------------------------------------------------
+  // format() - String scalars
+  // ---------------------------------------------------------------
+
+  @Test void format_string_isQuoted()
+  {
+    assertEquals("\"hi\"", ValueFormatter.format("hi", javaLang()));
+    assertEquals("\"hi\"", ValueFormatter.format("hi", pythonLang()));
+    assertEquals("\"hi\"", ValueFormatter.format("hi", scalaLang()));
+  }
+
+  // ---------------------------------------------------------------
+  // format() - Vector<Integer>
+  // ---------------------------------------------------------------
+
+  @Test void format_vectorOfInteger_isNormalizedThenFormatted()
+  {
+    Vector<Integer> vec = new Vector<>();
+    vec.add(9);
+    vec.add(8);
+    assertEquals("{9,8}", ValueFormatter.format(vec, javaLang()));
+  }
+
+  // ---------------------------------------------------------------
+  // normalize() - passthrough / idempotence
+  // ---------------------------------------------------------------
+
+  @Test void normalize_null_isNull() { assertNull(ValueFormatter.normalize(null)); }
+
+  @Test void normalize_intArray_isUnchanged()
+  {
+    int[] input = {7, 8};
+    assertArrayEquals(input, (int[])ValueFormatter.normalize(input));
+  }
+
+  @Test void normalize_plainScalar_isUnchanged() { assertEquals(5, ValueFormatter.normalize(5)); }
+
+  // ---------------------------------------------------------------
+  // Helpers - shorten how to retrive the programming language
+  // ---------------------------------------------------------------
+
+  private ProgrammingLanguage javaLang() { return Game.getInstance().programmingLanguageManager.JAVA; }
+  private ProgrammingLanguage scalaLang() { return Game.getInstance().programmingLanguageManager.SCALA; }
+  private ProgrammingLanguage pythonLang() { return Game.getInstance().programmingLanguageManager.PYTHON; }
+}
